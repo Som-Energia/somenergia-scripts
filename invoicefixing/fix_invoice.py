d7f531b13f1bfced1dfb2682988355167c4ba3be
abbse <aleix.badia@somenergia.coop>
2016-01-19 16:40:54 +0100

    IMP Consider multiple counters (old,new)

--- invoicefixing/fix_invoice.py
+++ invoicefixing/fix_invoice.py
@@ -46,112 +46,113 @@ def fix_contract(O, polissa_id, quarantine, start_date=None, end_date=None):
     out = ''
     fields_to_search = [('polissa', '=', polissa_id)]
     fields_to_read = ['active', 'data_alta', 'data_baixa']
-    meters = get_meters(O, fields_to_search, fields_to_read)
+    meters = get_meters(O, fields_to_search, fields_to_read, False)
 
     if not isinstance(meters, list):
         meters = [meters]
 
-    fields_to_search = [('comptador.polissa', '=', polissa_id)]
-    if start_date:
-        fields_to_search.append(('name', '>=', start_date))
-    if end_date:
-        fields_to_search.append(('name', '<=', end_date))
+    for meter in meters:
+        fields_to_search = [('comptador', '=', meter['id'])]
+        if start_date:
+            fields_to_search.append(('name', '>=', start_date))
+        if end_date:
+            fields_to_search.append(('name', '<=', end_date))
 
-    fields_to_read = ['name', 'comptador', 'periode', 'lectura', 'origen_id', 'observacions']
-    lects = get_measures(O, fields_to_search, fields_to_read, pool=False, active_test=False)
-    lects = remove_modmeter_lect(meters, lects)
-    out += show_invoice(polissa_id, lects, None, 10)
+        fields_to_read = ['name', 'comptador', 'periode', 'lectura', 'origen_id', 'observacions']
+        lects = get_measures(O, fields_to_search, fields_to_read, pool=False, active_test=False)
+        #lects = remove_modmeter_lect(meters, lects)
+        out += show_invoice(polissa_id, lects, None, 10)
 
-    if len(lects) < 2:
-        return
+        if len(lects) < 2:
+            return
 
-    offset_ct = {'2.0A (P1)': 1, '2.1A (P1)': 1,
-                 '2.0DHA (P1)': 2, '2.1DHA (P1)': 2}
+        offset_ct = {'2.0A (P1)': 1, '2.1A (P1)': 1,
+                     '2.0DHA (P1)': 2, '2.1DHA (P1)': 2}
 
-    for back_idx in reversed(range(1, min(len(lects), 15))):
-        if not (lects[0]['periode'][1].startswith('2')) or (lects[0]['periode'][1].endswith('DHS')):
-            # 3.0 and DHS pending
-            continue
+        for back_idx in reversed(range(1, min(len(lects), 15))):
+            if not (lects[0]['periode'][1].startswith('2')) or (lects[0]['periode'][1].endswith('DHS')):
+                # 3.0 and DHS pending
+                continue
 
-        offset = offset_ct[lects[0]['periode'][1]]
-        try:
-            def check_origen(lects, n, origen, start, offset):
-                for idx in range(start, n+1, offset):
-                    if not lects[idx]['origen_id'][1] == origen:
-                        return False
-                return True
+            offset = offset_ct[lects[0]['periode'][1]]
+            try:
+                def check_origen(lects, n, origen, start, offset):
+                    for idx in range(start, n+1, offset):
+                        if not lects[idx]['origen_id'][1] == origen:
+                            return False
+                    return True
 
-            prev_idx = back_idx*offset
-            last_idx = offset + prev_idx
+                prev_idx = back_idx*offset
+                last_idx = offset + prev_idx
 
-            offset_status = [check_origen(lects, prev_idx, "Estimada", offset, offset),
-                             check_origen(lects, prev_idx, "Estimada", offset+1, offset)]
+                offset_status = [check_origen(lects, prev_idx, "Estimada", offset, offset),
+                                 check_origen(lects, prev_idx, "Estimada", offset+1, offset)]
 
-            if ((offset_status[0]) \
-                and (lects[0]['lectura'] >= lects[last_idx]['lectura']) \
-                and (lects[0]['lectura'] < lects[prev_idx]['lectura'])) \
-                or ((offset == 2) \
-                and (offset_status[1]) \
-                and (lects[1]['lectura'] >= lects[last_idx+1]['lectura']) \
-                and (lects[1]['lectura'] < lects[prev_idx+1]['lectura'])):
+                if ((offset_status[0]) \
+                    and (lects[0]['lectura'] >= lects[last_idx]['lectura']) \
+                    and (lects[0]['lectura'] < lects[prev_idx]['lectura'])) \
+                    or ((offset == 2) \
+                    and (offset_status[1]) \
+                    and (lects[1]['lectura'] >= lects[last_idx+1]['lectura']) \
+                    and (lects[1]['lectura'] < lects[prev_idx+1]['lectura'])):
 
-                n_days_02 = (datetime.strptime(lects[0]['name'], '%Y-%m-%d') -
-                             datetime.strptime(lects[last_idx]['name'], '%Y-%m-%d')).days
+                    n_days_02 = (datetime.strptime(lects[0]['name'], '%Y-%m-%d') -
+                                 datetime.strptime(lects[last_idx]['name'], '%Y-%m-%d')).days
 
-                for day_idx in range(1, back_idx+1):
-                    _prev_idx = offset+(back_idx-day_idx)*offset
-                    _last_idx = offset+(back_idx-day_idx+1)*offset
-                    kWh_day_db = get_contract_daily_consumption(O, polissa_id)
+                    for day_idx in range(1, back_idx+1):
+                        _prev_idx = offset+(back_idx-day_idx)*offset
+                        _last_idx = offset+(back_idx-day_idx+1)*offset
+                        kWh_day_db = get_contract_daily_consumption(O, polissa_id)
 
-                    if offset_status[0]:
-                        # 2.XA
-                        quarantine['kWh'] += fix_measure(O, polissa_id, lects, last_idx, _last_idx, _prev_idx,
-                                                         0, kWh_day_db['P1'], n_days_02)
+                        if offset_status[0]:
+                            # 2.XA
+                            quarantine['kWh'] += fix_measure(O, polissa_id, lects, last_idx, _last_idx, _prev_idx,
+                                                             0, kWh_day_db['P1'], n_days_02)
 
-                    if offset == 2 and offset_status[1]:
-                        # 2.XDHA
-                        quarantine['kWh'] += fix_measure(O, polissa_id, lects, last_idx, _last_idx, _prev_idx,
-                                                         1, kWh_day_db['P2'], n_days_02)
+                        if offset == 2 and offset_status[1]:
+                            # 2.XDHA
+                            quarantine['kWh'] += fix_measure(O, polissa_id, lects, last_idx, _last_idx, _prev_idx,
+                                                             1, kWh_day_db['P2'], n_days_02)
 
 
-                    out += show_invoice(polissa_id, lects, 'Fixed', 10)
-                    invoice_date_end = lects[_prev_idx]['name']
-                    invoice_date_start = lects[_last_idx]['name']
-                    search_pattern = [('polissa_id', '=', polissa_id),
-                                      ('type', '=', 'out_invoice'),
-                                      ('data_final', '=', invoice_date_end),
-                                      ('data_inici', '=', invoice_date_start)]
-                    invoice_rectified_id = get_invoices(O, search_pattern, None, True)
-
-                    if not invoice_rectified_id:
-                        invoice_date_start = datetime.strftime(datetime.strptime(lects[_last_idx]['name'],
-                                                                                 '%Y-%m-%d') + timedelta(days=1), '%Y-%m-%d')
+                        out += show_invoice(polissa_id, lects, 'Fixed', 10)
+                        invoice_date_end = lects[_prev_idx]['name']
+                        invoice_date_start = lects[_last_idx]['name']
                         search_pattern = [('polissa_id', '=', polissa_id),
                                           ('type', '=', 'out_invoice'),
                                           ('data_final', '=', invoice_date_end),
                                           ('data_inici', '=', invoice_date_start)]
                         invoice_rectified_id = get_invoices(O, search_pattern, None, True)
 
-                    if invoice_rectified_id:
-                        invoices_id = pay_invoice(O, invoice_rectified_id[0], True)
-                        if invoices_id:
-                            fields_to_read= ['invoice_id',
-                                             'data_inici',
-                                             'data_final',
-                                             'amount_total']
-                            invoices = read_invoices(O, invoices_id, fields_to_read)
-                            invoice_original = invoices[1]['amount_total']
-                            invoice_rectified = invoices[0]['amount_total']
-                            diff = invoice_original - invoice_rectified
+                        if not invoice_rectified_id:
+                            invoice_date_start = datetime.strftime(datetime.strptime(lects[_last_idx]['name'],
+                                                                                     '%Y-%m-%d') + timedelta(days=1), '%Y-%m-%d')
+                            search_pattern = [('polissa_id', '=', polissa_id),
+                                              ('type', '=', 'out_invoice'),
+                                              ('data_final', '=', invoice_date_end),
+                                              ('data_inici', '=', invoice_date_start)]
+                            invoice_rectified_id = get_invoices(O, search_pattern, None, True)
 
-                            if diff > 2*get_contract_amount_mean(O, polissa_id):
-                                quarantine['euro'].append(polissa_id)
+                        if invoice_rectified_id:
+                            invoices_id = pay_invoice(O, invoice_rectified_id[0], True)
+                            if invoices_id:
+                                fields_to_read= ['invoice_id',
+                                                 'data_inici',
+                                                 'data_final',
+                                                 'amount_total']
+                                invoices = read_invoices(O, invoices_id, fields_to_read)
+                                invoice_original = invoices[1]['amount_total']
+                                invoice_rectified = invoices[0]['amount_total']
+                                diff = invoice_original - invoice_rectified
 
-                            out += show_results(invoices, quarantine)
+                                if diff > 2*get_contract_amount_mean(O, polissa_id):
+                                    quarantine['euro'].append(polissa_id)
 
-        except Exception, e:
-            print e
-            pass
+                                out += show_results(invoices, quarantine)
+
+            except Exception, e:
+                print e
+                pass
     return out
 
 
