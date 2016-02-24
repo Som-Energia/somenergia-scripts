c01e70c58d795bd371881a39839f9d3218ae126a
proig <pere.roig@somenergia.coop>
2016-02-24 17:22:38 +0100

    gitignore

--- Informacio/General_Quadre_control.py
+++ Informacio/General_Quadre_control.py
@@ -3,34 +3,47 @@
 from ooop import OOOP
 from datetime import datetime
 import configdb
+from sys import argv
 
 O = OOOP(**configdb.ooop)
 
 pol_obj = O.GiscedataPolissa
+sw_obj = O.GiscedataSwitching
 
 data = str(input("Escriu la data fins que volem tenir la informació (inclosa) amb format (amb les cometes!) 'aaaa-mm-dd' :  "))
 if not(data):
     data = datetime.today().strftime('%Y-%m-%d')
 
+data_search = ('data_firma_contracte','<=',data)
+
 solicituts_erronies = len(pol_obj.search([('data_firma_contracte','=',False)]))
-
-solicituts = len(pol_obj.search([('data_firma_contracte','<=',data)]))
-
-solicituts_30A = len(pol_obj.search([('tarifa','=','3.0A'),('data_firma_contracte','<=',data)]))
-activats_30A = len(pol_obj.search([('tarifa','=','3.0A'),('data_firma_contracte','<=',data),('state','=','activa')]))
-
-factures_endarrerides = len(pol_obj.search([('facturacio_endarrerida','=',True),]))
-baixes = len(pol_obj.search([('active','=',0),('data_baixa','<=',data)]))
-
-activats = len(pol_obj.search([('data_alta','<=',data),('state','=','activa')]))
-
-
-acceptats = len(O.GiscedataSwitching.search([('proces_id.name','in',['C1','C2']),('state','=','open'),('rebuig','=',False),('step_id.name','=','02')]))
-
-activats_oberts = len(O.GiscedataSwitching.search([('proces_id.name','in',['C1','C2']),('state','=','open'),('step_id.name','in',['05','07'])]))
-
+solicituts = len(pol_obj.search([data_search]))
+solicituts_30A = len(pol_obj.search([('tarifa','=','3.0A'),data_search]))
+activats_30A = len(pol_obj.search([('tarifa','=','3.0A'),
+                                    ('state','=','activa'),
+                                    data_search]))
+factures_endarrerides = len(pol_obj.search([('facturacio_endarrerida','=',True),
+                                            data_search]))
+baixes = len(pol_obj.search([('active','=',0),
+                    ('data_baixa','<=',data)]))
+activats = len(pol_obj.search([('data_alta','<=',data),
+                            ('state','=','activa')]))
+acceptats = len(sw_obj.search([('proces_id.name','in',['C1','C2']),
+                               ('state','=','open'),
+                               ('rebuig','=',False),
+                               ('step_id.name','=','02')]))
+activats_oberts = len(sw_obj.search([('proces_id.name','in',['C1','C2']),
+                                    ('state','=','open'),
+                                    ('step_id.name','in',['05','07'])]))
 sol_acceptades = activats + acceptats + activats_oberts
-
 Sol_problematiques = solicituts-sol_acceptades
 
-print "Solicituds erronies %d\n Solicituts: %d\n Solicituts tarifa 3.0A: %d\n Activats 3.0A %d\n Contractes donats de baixa: %d \n Factures endaderrides: %d \n Solicituds problematiques: %d \n Contractes aceptats: %d \n Contractes activats: %d" % (solicituts_erronies, solicituts, solicituts_30A,activats_30A,baixes, factures_endarrerides,Sol_problematiques, sol_acceptades, activats)
+print "Solicituds erronies {solicituts_erronies}".format(**locals())
+print "Solicituts: {solicituts}".format(**locals())
+print "Solicituts tarifa 3.0A: {solicituts_30A}".format(**locals())
+print "Solicituts Activats 3.0A: {activats_30A}".format(**locals())
+print "Solicituts Contractes donats de baixa: {baixes}".format(**locals())
+print "Solicituts Factures endaderrides: {factures_endarrerides}".format(**locals())
+print "Solicituts Solicituds problematiques: {Sol_problematiques}".format(**locals())
+print "Solicituts Contractes aceptats: {sol_acceptades}".format(**locals())
+print "Solicituts Contractes activats: {activats}".format(**locals())
