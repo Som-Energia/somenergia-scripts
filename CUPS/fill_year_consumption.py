8bd0ba691c5231704ad5255eb25ae2bd17b58209
abbse <aleix.badia@somenergia.coop>
2016-03-21 12:46:57 +0100

    Added CUPS consumption refilling

---
+++ CUPS/fill_year_consumption.py
@@ -0,0 +1,10 @@
+from ooop import OOOP
+import configdb
+
+O=OOOP(**configdb.ooop)
+
+fields_to_search = [('state','=','activa')]
+contracts_id = O.GiscedataPolissa.search(fields_to_search)
+for contract_id in contracts_id:
+    cups_id = O.GiscedataPolissa.read(contract_id,['cups'])['cups'][0]
+    O.GiscedataCupsPs.omple_consum_anual(cups_id)
