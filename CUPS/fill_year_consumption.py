893333e506f0d42e41da7f1b94cce9681b5455d9
abbse <aleix.badia@somenergia.coop>
2016-03-31 15:39:21 +0200

    Fixed API call

--- CUPS/fill_year_consumption.py
+++ CUPS/fill_year_consumption.py
@@ -7,4 +7,4 @@ fields_to_search = [('state','=','activa')]
 contracts_id = O.GiscedataPolissa.search(fields_to_search)
 for contract_id in contracts_id:
     cups_id = O.GiscedataPolissa.read(contract_id,['cups'])['cups'][0]
-    O.GiscedataCupsPs.omple_consum_anual(cups_id)
+    O.GiscedataCupsPs.omple_consum_anual_xmlrpc(cups_id)
