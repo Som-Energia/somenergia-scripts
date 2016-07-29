7bf48a9ed7a98dfab1424dfe6bb425055dd48360
César López Ramírez <cesar.lopez@somenergia.coop>
2016-07-29 12:58:53 +0200

    Fix origin_id bug in energy invoice readings WIP

--- invoicefixing/draftinvoices.sql
+++ invoicefixing/draftinvoices.sql
@@ -88,18 +88,23 @@ LEFT JOIN (
     GROUP BY polissa_id
 ) past_invoices ON past_invoices.polissa_id = factura.polissa_id
 LEFT JOIN (
-    SELECT
-        bool_or(origen_id IN (7,9,10,11)) AS ultima_estimada,
-        factura_id
-    FROM
-        giscedata_facturacio_lectures_energia
-    GROUP BY
-		factura_id
-) lecturas ON lecturas.factura_id=factura.id AND ultima_estimada
+    select bool_or(gll.origen_id in (7,9,10,11)) as ultima_estimada,factura_id
+    from giscedata_facturacio_lectures_energia gfle
+    inner join product_template pt_periode
+	on pt_periode.name = substring(gfle.name from '%#(#"%#"#)%' for '#')
+    inner join giscedata_polissa_tarifa gpt_tarifa
+	on gpt_tarifa.name = substring(gfle.name from '#"%#"_#(%' for '#')
+    inner join giscedata_polissa_tarifa_periodes gptp
+	on gptp.product_id = pt_periode.id and gpt_tarifa.id = gptp.tarifa
+    inner join giscedata_lectures_lectura gll
+	on gll.periode = gptp.id 		-- Mismo periodo
+    where gptp.tipus='te' 			-- Lectura energía
+	and gll.comptador=gfle.comptador_id 	-- Mismo contador
+	and gll.name=gfle.data_actual		-- Misma fecha
+    group by factura_id) lecturas ON lecturas.factura_id=factura.id AND ultima_estimada
 WHERE
     lot.state = 'obert' AND
     invoice.state = 'draft' AND
     invoice.type = 'out_invoice' AND
     TRUE
 ;
-
