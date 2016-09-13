6263d89dedc34494742c12db37494a3e78db33d6
abbse <aleix.badia@somenergia.coop>
2016-09-13 10:22:22 +0200

    Updated fill consumption in order to support both older and newer contracts

--- CUPS/fill_year_consumption.py
+++ CUPS/fill_year_consumption.py
@@ -1,10 +1,49 @@
+import sys
+import getopt
 from ooop import OOOP
 import configdb
 
 O=OOOP(**configdb.ooop)
 
-fields_to_search = [('state','=','activa')]
-contracts_id = O.GiscedataPolissa.search(fields_to_search)
-for contract_id in contracts_id:
-    cups_id = O.GiscedataPolissa.read(contract_id,['cups'])['cups'][0]
-    O.GiscedataCupsPs.omple_consum_anual_xmlrpc(cups_id)
+def main(argv):
+    updatehistory=False
+    try:
+        opts, args = getopt.getopt(argv, 'hu', ['help','updatehistory'])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            sys.exit()
+        elif opt in ('-u', '--updatehistory'):
+            updatehistory=True
+    failed=[]
+
+    print 'Reading CUPS to be updated'
+    fields_to_search = [('state','=','activa')]
+    contracts_id = O.GiscedataPolissa.search(fields_to_search)
+    contracts_cups_id=[]
+    for contract in O.GiscedataPolissa.read(contracts_id,['cups']):
+        try:
+            contracts_cups_id.append(contract['cups'][0])
+        except:
+            failed.append(('cupsread',cups_id,str(e)))
+
+    # Default update cups with no previous history based calculation
+    cups_search=[('conany_origen','!=','lectures')]
+    if updatehistory:
+        # Update cups with previous history based calculation
+        cups_search = [('conany_origen','=','lectures')]
+    cups_search += [('id','in',contracts_cups_id)]
+    toupdate_cups_id=O.GiscedataCupsPs.search(cups_search)
+
+    print "CUPS to be updated", len(toupdate_cups_id)
+    for cups_id in toupdate_cups_id:
+        try:
+            O.GiscedataCupsPs.omple_consum_anual_xmlrpc(cups_id)
+        except Exception as e:
+            failed.append(('cupsupdate',cups_id,str(e)))
+
+    print "Failed ", failed
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
