906950ec1bd1f5d0980116d10344f9f1b7d844ed
proig <pere.roig@somenergia.coop>
2017-02-09 15:35:32 +0100

    Kill "Ja existeix una factura amb el mateix.." too

--- Importacions_F1_Q1/Fact_impF1_eliminar_Ja_existeix.py
+++ Importacions_F1_Q1/Fact_impF1_eliminar_Ja_existeix.py
@@ -8,6 +8,7 @@ O = OOOP(**configdb.ooop)
 imp_obj = O.GiscedataFacturacioImportacioLinia
 
 imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like',"Aquest fitxer XML ja s'ha processat en els següents IDs")])
+imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like',"Ja existeix una factura amb el mateix origen")])
 #imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like','XML erroni')])
 imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like',"XML no es correspon al tipus F1")])
 imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like',"Document invàlid")])
