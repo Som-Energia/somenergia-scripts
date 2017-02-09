ae98a9b01d26dac92e1c0c7e2c950b5a16d30ef3
Pere Roig <pere.roig@somenergia.coop>
2017-02-09 17:50:08 +0100

    Refactoring and improve with import_ok()

--- fix_modcontractual_orig.py
+++ fix_modcontractual_orig.py
@@ -7,8 +7,7 @@ import argparse
 O = OOOP(**configdb.ooop)
 
 #Objectes
-wiz_obj = O.GiscedataFacturacioSwitchingWizard
-imp_obj = O.GiscedataFacturacioImportacioLinia
+lin_obj = O.GiscedataFacturacioImportacioLinia
 pol_obj = O.GiscedataPolissa
 mod_obj = O.GiscedataPolissaModcontractual
 
@@ -16,8 +15,8 @@ mod_obj = O.GiscedataPolissaModcontractual
 now = datetime.now()
 start_date = (now - timedelta(days=14)).strftime('%Y-%m-%d')
 
-erronis_ids = imp_obj.search([('state','=','erroni'),('info','like',': La tarifa ('),('data_carrega','>', start_date)])
-cups_erronis = imp_obj.read(erronis_ids,['cups_id','distribuidora'])
+erronis_ids = lin_obj.search([('state','=','erroni'),('info','like',': La tarifa ('),('data_carrega','>', start_date)])
+cups_erronis = lin_obj.read(erronis_ids,['cups_id','distribuidora'])
 cups = []
 for cups_id in cups_erronis:
     if cups_id['cups_id']:
@@ -25,31 +24,33 @@ for cups_id in cups_erronis:
 pol_ids = pol_obj.search([('cups.name','in',cups)])
 
 #Comptadors i taules
-imp_sense_canvis = []
-imp_amb_canvi = []
+lin_sense_canvis = []
+lin_amb_canvi = []
 errors = []
 total_erronis = len(erronis_ids)
 total= len(pol_ids)
 count = 0
 
-def reimportar_F1(cups_id):
-    imp_obj = O.GiscedataFacturacioImportacioLinia
-    wiz_obj = O.GiscedataFacturacioSwitchingWizard
-    
-    vals_search = [('state','=','erroni'),('cups_id','=',cups_id)]
-    imp_ids = imp_obj.search(vals_search)
-    for imp_id in imp_ids:
-        imp_read = O.GiscedataFacturacioImportacioLinia.read(imp_id,['info'])
-        ctx = {'active_id':imp_id, 'fitxer_xml': True}
-        wz_id = wiz_obj.create({}, ctx)
-        wiz = wiz_obj.get(wz_id)
-        wiz.action_importar_f1(ctx)
-        imp_new_id = O.GiscedataFacturacioImportacioLinia.read(imp_id,['info'])
-        if imp_read['info'] == imp_new_id['info']:
-            return False
-        else:
-            return False
-
+def reimportar_ok(linia_id):
+    import time
+    lin_obj = O.GiscedataFacturacioImportacioLinia
+    info_inicial = lin_obj.read([linia_id],['info'])[0]['info']
+    lin_obj.process_line(linia_id)
+    time.sleep(15)
+    lin_read = lin_obj.read([linia_id],['info','conte_factures'])
+    info_nova = lin_read[0]['info']
+    conte_factures = lin_read[0]['conte_factures']
+    value = {'mateix_missatge':False,'ok':False}
+    if lin_read[0]['conte_factures']:
+        value['ok'] = True
+    if info_inicial == info_nova:
+        #print "informacio igual: %s" % info_inicial
+        print "Mateix missatge"
+        value['mateix_missatge']=True
+    else:
+        #print "Missatge Inicial: %s \n Missatge Final: %s" % (info_inicial,info_nova)
+        print "S'ha actualitzat el missatge"
+    return value
 
 # Pot ser que una polissa tingui mes d'una factura per arreglar, d'aixo la diferencia entres els dos numeros.
 print " ######### \n De les %s importacions erronies hem trobat %s polisses per arreglar\n ######### \n" % (total_erronis, total)
@@ -76,36 +77,27 @@ for pol_id in pol_ids:
                 data_inici = datetime.strftime(data_inici + timedelta(a),'%Y-%m-%d')
                 print "%s-->%s" % (mod_actual.data_inici,data_inici)
                 mod_actual.write({'data_inici': data_inici})
-                
-        
-                
+
                 ##-- Re-importem el F1 --##
-                imp_id = imp_obj.search([('state','=','erroni'),('cups_id.name','=',mod_actual.cups.name),('info','like',': La tarifa (')])
-                imp_ = imp_obj.read(imp_id,['info','conte_factures'])
+                lin_ids = lin_obj.search([('state','=','erroni'),('cups_id.name','=',mod_actual.cups.name),('info','like',': La tarifa (')])
                 ##### Pot ser que hi hagi mes d'una factura encallada, PENSAR EN QUIN ORDRE LES VOLEM ARREGLAR!!
-                if len(imp_id)>1:
-                    print "te mes d'uan factura encallada"
+                if len(lin_ids)>1:
+                    print "te mes d'una factura encallada"
                 mod_data=False
-                for imp in imp_:
-                    info_inicial = imp['info']
-                    ctx = {'active_id':imp['id'], 'fitxer_xml': True}
-                    wz_id = wiz_obj.create({}, ctx)
-                    wiz = wiz_obj.get(wz_id)
-                    wiz.action_importar_f1(ctx)
-                
-                    imp_nou = O.GiscedataFacturacioImportacioLinia.read(imp['id'],['info','conte_factures'])
-                    if imp_nou['conte_factures']:
+                for lin_id in lin_ids:
+                    reimportacio = reimportar_ok(lin_id)
+                    if reimportacio['ok']:
                         print "Factura importada correctament! Polissa: %s" % mod_contractuals['name']
-                        imp_amb_canvi.append(mod_contractuals['name'])
+                        lin_amb_canvi.append(mod_contractuals['name'])
+                        break
+                    if reimportacio['mateix_missatge']:
+                        print "Informacio igual. Tornem a posar les dates de les modificacions tal i com estaven"
+                        lin_sense_canvis.append(lin_id)
                     else:
-                        if info_inicial == imp_nou['info']:
-                            print "Informacio igual. Tornem a posar les dates de les modificacions tal i com estaven"
-                            imp_sense_canvis.append(imp['id'])
-                        else:
-                            print "Missatges diferents: Info final  : %s. Tornem a posar les dates de les modificacions tal i com estaven" % imp_nou['info']
+                        print "Missatges diferents: Info final  : %s. Tornem a posar les dates de les modificacions tal i com estaven" % lin_nou['info']
                         ## -- tornem a posar les dates com estaven inicialment --##
                         mod_antiga.write({'data_final': data_final_inicial})
-                        mod_actual.write({'data_inici': data_inicial})         
+                        mod_actual.write({'data_inici': data_inicial})
         else: 
             print "La polissa %s te %s modficacions contractuals, no hem fet cap canvi" % (mod_contractuals['name'],len(mod_contractuals['modcontractuals_ids']))
             ### si el num de modificacions es igual a 1, cnaviar tarifa i reimportar, sino funciona, tornar tot com estava.
@@ -115,12 +107,12 @@ for pol_id in pol_ids:
         errors.append({pol_id:e})
         print e
         
-erronis_finals_id = imp_obj.search([('state','=','erroni'),('info','like',': La tarifa (')])
+erronis_finals_id = lin_obj.search([('state','=','erroni'),('info','like',': La tarifa (')])
 erronis_finals = len(erronis_finals_id)
-total_imp = len(imp_amb_canvi)
+total_lin = len(lin_amb_canvi)
 
 ##-- Imprimim per pantalla els esultats dels script --##
 print "\nHi havia %d amb importacions erronies per intentar arreglar del total (%s) de les importacions erronies" % (total, total_erronis)
 print "Despres del script, hi ha %d importacions erronies amb missatge 'tarifa incoherent'" % erronis_finals
-print "S'han importat be %d arxius xml" % (total_imp)
+print "S'han importat be %d arxius xml" % (total_lin)
 print "Errors: {}".format(len(errors))
