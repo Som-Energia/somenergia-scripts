c27b3658ea01ff53de4e026ace4c1beaa7783df3
Pere Roig <pere.roig@somenergia.coop>
2017-02-13 11:38:10 +0100

    Improving function Reimportar

--- Importacions_F1_Q1/Reimportar_XML_F1.py
+++ Importacions_F1_Q1/Reimportar_XML_F1.py
@@ -27,23 +27,33 @@ def parseargs():
         )
     return parser.parse_args()
 
-def output(total, erronis_finals):
+def output(total,lin_factura_generada,lin_mateix_missatge,lin_diferent_missatge):
     print "Importacions erronies inicials: %d" % total
-    print "Importacions correctes:  %d" % (total-erronis_finals)
-    print "Importacions encara erronies: %d" % erronis_finals
+    print "Reimportacions que han generat factura:  %d" % len(lin_factura_generada)
+    print "Importacions encara erronies: %d" % (len(lin_diferent_missatge)+len(lin_mateix_missatge))
+    print "  - Amb el mateix missatge: %d" % len(lin_mateix_missatge)
+    print "  - Amb un missatge diferent: %d" % len(lin_diferent_missatge)
 
 def reimportar_ok(linia_id):
+    import time
+    lin_obj = O.GiscedataFacturacioImportacioLinia
     info_inicial = lin_obj.read([linia_id],['info'])[0]['info']
     lin_obj.process_line(linia_id)
     time.sleep(15)
-    info_nova = lin_obj.read([linia_id],['info'])[0]['info']
+    lin_read = lin_obj.read([linia_id],['info','conte_factures'])
+    info_nova = lin_read[0]['info']
+    conte_factures = lin_read[0]['conte_factures']
+    value = {'mateix_missatge':False,'ok':False}
+    if lin_read[0]['conte_factures']:
+        value['ok'] = True
     if info_inicial == info_nova:
         #print "informacio igual: %s" % info_inicial
         print "Mateix missatge"
-        return False
-    #print "Missatge Inicial: %s \n Missatge Final: %s" % (info_inicial,info_nova)
-    print "S'ha actualitzat el missatge"
-    return True
+        value['mateix_missatge']=True
+    else:
+        #print "Missatge Inicial: %s \n Missatge Final: %s" % (info_inicial,info_nova)
+        print "S'ha actualitzat el missatge"
+    return value
 
 args=parseargs()
 if not args.cups and not args.info and not args.date:
@@ -70,27 +80,31 @@ if args.date:
     if data_carrega:
         vals_search += [('data_carrega','>',data_carrega)]
 
-erronis_ids = lin_obj.search(vals_search)
+lin_ids = lin_obj.search(vals_search)
 
 #comptadors
 count=0
-total=len(erronis_ids)
+total=len(lin_ids)
 
 print "Hi ha %d amb importacions erronies inicials" % total
 
 #Incialitzem comptadors
-imp_sense_canvis = []
-imp_amb_canvi = []
+lin_factura_generada = []
+lin_mateix_missatge = []
+lin_diferent_missatge = []
 
-for linia_id in erronis_ids:
+for lin_id in lin_ids:
     count+=1
-    if reimportar_ok(linia_id):
-        imp_sense_canvis.append(linia_id)
+    reimportacio = reimportar_ok(lin_id)
+    if reimportacio['ok']:
+        print "Factura importada correctament!"
+        lin_factura_generada.append(lin_id)
+        break
+    if reimportacio['mateix_missatge']:
+        lin_mateix_missatge.append(lin_id)
     else:
-        imp_amb_canvi.append(linia_id)
+        lin_diferent_missatge.append(lin_id)
     print "%d/%d"%(count,total)
-erronis_finals_ids = lin_obj.search(vals_search)  
-erronis_finals = len(erronis_finals_ids)
 
-output(total,erronis_finals)
+output(total,lin_factura_generada,lin_mateix_missatge,lin_diferent_missatge)
 
