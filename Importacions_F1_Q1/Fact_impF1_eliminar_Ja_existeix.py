9e863a8bf71965fe9efcb79200b13040a4809df3
Alberto Rasillo <bluesnatural@gmail.com>
2017-02-23 11:29:18 +0100

    test modify

--- Importacions_F1_Q1/Fact_impF1_eliminar_Ja_existeix.py
+++ Importacions_F1_Q1/Fact_impF1_eliminar_Ja_existeix.py
@@ -11,16 +11,18 @@ imp_obj = O.GiscedataFacturacioImportacioLinia
 #imp_del_ids = imp_obj.search([('state','=','erroni'),('info','=','Ja existeix una factura amb el mateix origen')])
 
 
-imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like','Ja existeix una factura')])
-imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like','XML erroni')])
+#imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like','Ja existeix una factura')])
+imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like','XML erroni')])
 imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like',"XML no es correspon al tipus F1")])
 imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like',"Document invàlid")])
+im_del_ids_reads = O.GiscedataFacturacioImportacioLinia.read(imp_del_ids,['state','info'])
 
 total = len(imp_del_ids)
 n = 0
 
 for imp_del_id in imp_del_ids:
     try:
+        print im_del_ids_reads
         imp_obj.unlink([imp_del_id])
         n +=1
         print "%d/%d" % (n,total)
