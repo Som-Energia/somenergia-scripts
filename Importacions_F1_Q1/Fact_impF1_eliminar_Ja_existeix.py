d497afbf1b264e21ca0d3f52e115b528977699b2
Alberto Rasillo <bluesnatural@gmail.com>
2017-02-24 11:05:24 +0100

    fix error 48 invoices can't delete

--- Importacions_F1_Q1/Fact_impF1_eliminar_Ja_existeix.py
+++ Importacions_F1_Q1/Fact_impF1_eliminar_Ja_existeix.py
@@ -7,25 +7,23 @@ O = OOOP(**configdb.ooop)
 
 imp_obj = O.GiscedataFacturacioImportacioLinia
 
-#imp_del_ids = imp_obj.search([('state','=','erroni'),('info','=','Aquest fitxer XML ja s\'ha processat en els següents IDs')])
-#imp_del_ids = imp_obj.search([('state','=','erroni'),('info','=','Ja existeix una factura amb el mateix origen')])
+imp_ids_no_del = [470571, 468790, 462576, 459709, 456920, 455680, 458431, 457376, 448841, 446859, 446838, 445293, 445102, 433834, 435880, 435575, 443804, 441518, 435379, 436044, 435699, 428398, 393928, 366042, 351351, 348093, 324194, 310987, 279511, 262881, 234356, 218342, 221200, 220413, 217773, 220530, 215933, 218237, 219422, 221949, 214971, 213972, 213157, 185373, 157501, 156467, 106294, 27995]
 
-
-imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like','Ja existeix una factura')])
-imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like','XML erroni')])
+imp_del_ids = imp_obj.search([('state','=','erroni'),('info','like','Ja existeix una factura'),('id','not in', imp_ids_no_del)])
+#imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like','XML erroni')])
 imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like',"XML no es correspon al tipus F1")])
 imp_del_ids += imp_obj.search([('state','=','erroni'),('info','like',"Document invàlid")])
-#im_del_ids_reads = O.GiscedataFacturacioImportacioLinia.read(imp_del_ids,['state','info'])
+imp_del_ids_reads = O.GiscedataFacturacioImportacioLinia.read(imp_del_ids,['state','info'])
 
 total = len(imp_del_ids)
 n = 0
-#print total
 for imp_del_id in imp_del_ids:
     try:
-        #print im_del_ids_reads
         imp_obj.unlink([imp_del_id])
         n +=1
         print "%d/%d" % (n,total)
     except Exception, e:
         print e    
 
+
+print "S'han eliminat %d arxius" % n
