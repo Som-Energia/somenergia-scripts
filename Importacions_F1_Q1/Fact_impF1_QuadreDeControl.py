9f0ce7ff171e2a5af1713cdd76a21b4d55fe1ff3
Pere Roig <pere.roig@somenergia.coop>
2017-11-20 17:52:38 +0100

    Refactor with Templates Errors

--- Importacions_F1_Q1/Fact_impF1_QuadreDeControl.py
+++ Importacions_F1_Q1/Fact_impF1_QuadreDeControl.py
@@ -33,39 +33,17 @@ if args.date:
         vals_search += [('data_carrega','>=',data_carrega)]
 
 imp_obj = O.GiscedataFacturacioImportacioLinia
+et_obj = O.GiscedataFacturacioSwitchingErrorTemplate
 
-data_carrega = '2017-03-19'
-
-# Tipologies d'error. Diccionari amb string per humans i string per ERP
-tipificacio_errors = {
-        "A. TOTAL":'',
-        "B. Divergencies en lectures": 'Divergència',
-        "C. Sense Factura de referencia": 'trobat la factura de referència amb número',
-        "D. Data erronia modificacions contractuals": 'La tarifa (',
-        "E. Bloquejos amb altres importacions": 'deadlock',
-        "F. Temps d'importacio massa llarg": 'timeout',
-        "G. No es pot facturar a traves de lectures": ": No s'ha pogut facturar a trav",
-        "G. Inicial": ": inicial",
-        "G. Data inicial Comptador erronea": 'Error introduint lectures en data inicial',
-        "H. Data final Comptador erronea": 'Error introduint lectures en data fi',
-        "I. Sense pòlissa vinculada": 'lissa vinculada al cups',
-        "J. No es troba el CUPS indicat": 'trobat el CUPS',
-        "K. No es troba l'empresa emisora": 'Empresa emisora no correspon',
-        "L. No s'han trobat tots els periodes": 'identificar tots els períodes',
-        "M. Problemes amb la reactiva": "reactiva per facturar",
-        "N. Comptador diferent de la polissa": 'assignat el comptador',
-        "O. No existeix cap modificacio contractual": 'Error no existeix cap modificaci',
-        "P. Intent de dividir entre 0": "float division",
-        "R. No such field": 'child',
-        "\nS. F1 ja importat": "ja s'ha processat",
-        "T. XML erroni": 'XML no es correspon al tipus F1',
-        "U. Document invalid": 'Document invàlid',
-    }
-
+et_ids = et_obj.search([])
+templates_errors_read = et_obj.read(et_ids,['code','description'])
+list_errors = sorted([{int(a['code'])+int(a['phase'])*1000:a['description']} for a in et_obj.read(et_ids,['code','phase','description'])])
 
 print "ERRORS DE IMPORTACIONS de F1"
-for error,error_erp in sorted(tipificacio_errors.items()):
-    vals_search_f1 = vals_search + [('info', 'like', error_erp)]
+for a in list_errors:
+    vals_search_f1 = vals_search + [('info', 'like', a.keys())]
     erronis_ids = imp_obj.search(vals_search_f1)
-    print " -->" + " %s: %d" % (error,len(erronis_ids),)
+    for b,c in a.items():
+        print "  [" + str(b) + "]. Errors: ",str(len(erronis_ids))
+        print "  Descripció error: ", c , "\n"
 
