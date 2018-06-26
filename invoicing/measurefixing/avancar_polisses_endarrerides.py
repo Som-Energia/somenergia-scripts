a2a4c62d0fce39813b097fd8dc5b4eaf250e7ab3
Francisco Cubero <Francisco.Cubero@somenergia.coop>
2018-06-26 12:47:38 +0200

    major refactor over the script to unify criteria

--- invoicing/measurefixing/avancar_polisses_endarrerides.py
+++ invoicing/measurefixing/avancar_polisses_endarrerides.py
@@ -8,7 +8,6 @@ from validacio_eines import (
     lazyOOOP,
     draftContractInvoices,
     enviar_correu,
-    showContract,
     open_and_send,
 )
 from consolemsg import step, fail, success, warn, error
@@ -17,7 +16,7 @@ import ssl
 import sys
 
 
-#####workaround validate ssl testing
+# Workaround validate ssl testing
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
@@ -27,8 +26,35 @@ else:
     # Handle target environment that doesn't support HTTPS verification
     ssl._create_default_https_context = _create_unverified_https_context
 
+
+# Helper functions
+def debugWizard(wiz):
+    step('polissa_id', wiz.polissa_id)
+    step('data_inici', wiz.data_inici)
+    step('data_factura', wiz.data_factura)
+    step('data_ultima_lectura_original', wiz.data_ultima_lectura_original)
+    step('state:', wiz.state)
+    step('info:', wiz.info)
+
+def undoDraftInvoicesAndMeasures(polissa, invoice_ids, data_ultima_lectura_original):
+    step("\tEliminem factures creades {}", invoice_ids)
+    if invoice_ids:
+        Invoice.unlink(invoice_ids, {})
+    measures_ids = Measures.search([
+        ('comptador','in', polissa.comptadors),
+        ('name', '>', str(data_ultima_lectura_original)),
+    ])
+    step("\tEliminem lectures creades {}", measures_ids)
+    if measures_ids:
+        Measures.unlink(measures_ids, {})
+
+
+# constants
+DELAYED_CONTRACT_WARNING_TEXT = "F017"
+
 # Modification variable to safer execution
 doit = '--doit' in sys.argv
+direct = '--direct' in sys.argv
 
 # definitions
 step("Connectant a l'erp")
@@ -59,17 +85,25 @@ polisses = Contract.read(polissaEndarrerida_ids,[
     'data_ultima_lectura',
     'lot_facturacio',
     'pagador',
+    'cups',
     ])
 
 result = ns()
 result.contractsWithPreviousDraftInvoices=[]
 result.contractsWithError=[]
 result.contractsForwarded=[]
+result.contractsWarned=[]
 
 for counter,polissa in enumerate(polisses):
     polissa = ns(polissa)
-    step("{}/{} polissa {} ",counter, polissaEndarrerida_ids_len, polissa.name)
-    showContract(polissa.id)
+    success("{}/{} polissa: {}  id: {}  data ultima lectura: {}  CUPS: {}",
+        counter+1,
+        polissaEndarrerida_ids_len,
+        polissa.name,
+        polissa.id,
+        polissa.data_ultima_lectura,
+        polissa.cups[1],
+        )
 
     drafInvoice_ids = draftContractInvoices(polissa.id)
     if drafInvoice_ids:
@@ -78,87 +112,64 @@ for counter,polissa in enumerate(polisses):
         result.contractsWithPreviousDraftInvoices.append(polissa.id)
         continue
 
-    step("\tInstantiate wizard")
+    step("\tInicialtzem el wizard")
     Wizard = O.WizardAvancarFacturacio
     wizcontext = dict(active_id = polissa.id)
     aWizard = Wizard.create({}, wizcontext)
 
-    def debugWizard():
-        print 'polissa_id', aWizard.polissa_id
-        print 'data_inici', aWizard.data_inici
-        print 'data_factura', aWizard.data_factura
-        print 'data_ultima_lectura_original', aWizard.data_ultima_lectura_original
-        print 'state:', aWizard.state
-        print 'info:', aWizard.info
-
     data_inici_anterior = None
-
     while aWizard.data_inici != data_inici_anterior:
 
         data_inici_anterior = aWizard.data_inici
 
-        step("\tGenerando factura para {}", aWizard.data_inici)
+        step("\tGenerant factures per {}", aWizard.data_inici)
         aWizard.action_generar_factura()
-        step("\tState: {0.state}\nInfo:\n{0.info}",
-            aWizard)
+        step(u"\tState: {0.state}",aWizard)
+        step(u"\tInfo:")
+        print repr(aWizard.info)
 
         if aWizard.state != 'init': break
 
-    generatedInvoice_ids = draftContractInvoices(polissa.id)
-    success("\tFacturas generadas: {}", generatedInvoice_ids)
-    Invoice = O.GiscedataFacturacioFactura
+    step("\tValidem factures creades")
+    generated_invoice_ids = draftContractInvoices(polissa.id)
+    success("\tFactures generades: {}", generated_invoice_ids)
 
-    #step("TODO: Call giscedata factura validation validator") 
-    Validator = O.GiscedataFacturacioValidationValidator
-    warning = O.GiscedataFacturacioValidationWarning
-    step("\tValidando facturas...")
-    validation_errors = [
-        Validator.validate_invoice(invoice_id) 
-        for invoice_id in generatedInvoice_ids
-        ]
-    for invoice_errors in validation_errors:
-        for invoice_error in invoice_errors:
-            step("\tDetected errors: {}", warning.read(invoice_error, ['message','name'])['name'])    
-    step("\tValidation result {}", validation_errors)
-    # TODO: filter out 'enrarecida' validations
-    ko = any(validation_errors)
-    if ko:
-        error("Polissa que falla: {}", polissa.id)
-
-    def clearDraftInvoices(polissa, invoice_ids, data_ultima_lectura_original):
-        step("\tRemoving created invoices {}", invoice_ids)
-        Invoice.unlink(invoice_ids, {})
-        measures_ids = Measures.search([
-            ('comptador','in', polissa.comptadors),
-            ('name', '>', str(data_ultima_lectura_original)),
-        ])
-        step("\tRemoving created measures {}", measures_ids)
-        if measures_ids:
-            Measures.unlink(measures_ids, {})
+    ko = False
+    for generated_invoice_id in generated_invoice_ids:
+        validation_warnings = Validator.validate_invoice(generated_invoice_id) 
+        for validation_warning in validation_warnings:
+            v_warning_text = warning.read(validation_warnings, ['message','name'])['name']
+            warn("\tValidation error {} {}",generated_invoice_id,v_warning_text)
+            if v_warning_text != DELAYED_CONTRACT_WARNING_TEXT: 
+                ko = True
 
     if ko:
-        step("\tAnotate it as a failing case")
+        step("\tAnotem la polissa com a cas fallit {} {}",polissa.id,polissa.name)
         result.contractsWithError.append(polissa.id)
     else:
-        step("\tAnotate it as a forwarded case")
+        step("\tAnotem la polissa com a cas ok")
         result.contractsForwarded.append(polissa.id)
 
     if not doit or ko:
-        clearDraftInvoices(polissa, generatedInvoice_ids, aWizard.data_ultima_lectura_original)
+        undoDraftInvoicesAndMeasures(polissa, generated_invoice_ids, aWizard.data_ultima_lectura_original)
     else:
-        if len(generatedInvoice_ids)>1:
-            step("\tMore than one invoice, sending the warning email")
+        if len(generated_invoice_ids)>1:
+            step("\tMes d'una factura generada enviem el mail de Avis de multiples factures")
+            result.contractsWarned.append(polissa.id)
             enviar_correu(polissa.id, 71, 8,'giscedata.polissa')
-        step("Open and send all the invoices")
-        print 'polissa.id', polissa.id
+        step("Obrir i enviar totes les factures generades, updatarà la data ultima factura a polissa")
         lang = O.ResPartner.read(polissa.pagador[0], ['lang'])['lang']
         # TODO: What if this fails? Mails already sent!
-        open_and_send(generatedInvoice_ids, lang) 
+        open_and_send(generated_invoice_ids, lang) 
 
-    ignoreme = raw_input("Pulsa return para siguiente contrato")
+    if not direct:
+        warn("prem entrar per avançar el següent contracte")
+        ignoreme = raw_input("")
 
+success(u"")
+success(u" - FINAL - ")
+success(u"")
 success(result.dump())
-
 success(u"""\
 - Polisses avancades a data de lot:
     - {contractsForwarded} 
@@ -169,8 +180,5 @@ success(u"""\
 - Polisses que no s'ha pogut validar les factures generades:
     - {contractsWithError}
 """, **result)
-
-
-
 
 # vim: et ts=4 sw=4
