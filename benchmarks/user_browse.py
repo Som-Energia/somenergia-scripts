b77e6e94c18e35e1f36e92a3759ff12a09ff4534
David García Garzón <voki@canvoki.net>
2018-11-20 16:50:32 +0100

    parametrized nregires, using consolemsg

--- benchmarks/user_browse.py
+++ benchmarks/user_browse.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
 
 import erppeek
@@ -6,21 +7,31 @@ import dbconfig
 
 import time
 from yamlns import namespace as ns
+import sys
+from consolemsg import step, error, warn, success
 
+
+nregistres = sys.argv[1] if len(sys.argv)>1 else 100
+
+
+step("Connectant a {server}",**dbconfig.erppeek)
 t0 = time.time()
-
 erp = erppeek.Client(**dbconfig.erppeek)
 
+success("Establir connexió: {}", time.time()-t0)
+
+t0 = time.time()
 
 users = erp.ResPartner.read(range(100),[])
 
-print("Reading 100 Partners {}".format(time.time()-t0))
+success("Llegir {} Partners {}",nregistres, time.time()-t0)
 
 t0 = time.time()
 contracts = erp.GiscedataPolissa.read(range(100),[])
-print("Reading 100 Contracts {}".format(time.time()-t0))
+success("Llegir {} Contractes {}",nregistres, time.time()-t0)
 
-users = ns.loads(ns(users=users).dump())
+
+#users = ns.loads(ns(users=users).dump())
 #print users.users[2].dump()
 
 
