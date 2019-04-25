972479a3eedda75b31d55f0c788bb8eafc2db418
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2019-04-25 12:37:03 +0200

    minor changes in refres f1 code script

--- invoicing/measurefixing/refresh_orgin_f1.py
+++ invoicing/measurefixing/refresh_orgin_f1.py
@@ -22,6 +22,7 @@ f1line_len = len(f1line_ids)
 
 no_attachments = []
 many_codes = []
+no_codes = []
 updated = []
 
 for count,f1line_id in enumerate(f1line_ids):
@@ -37,17 +38,26 @@ for count,f1line_id in enumerate(f1line_ids):
 
     found_f1_codes = []
     for attachment in f1line._attachments_field:
-        f1_xml = base64.decodestring(attachment.datas_mongo)
-        f1_hits = re.findall('<NumeroFactura>.*</NumeroFactura>',f1_xml)
-        for f1_hit in f1_hits:
-            f1_number = f1_hit.replace('<NumeroFactura>','').replace('</NumeroFactura>','')
-            if f1_number:
-                found_f1_codes.append(f1_number)
+        try:
+            f1_xml = base64.decodestring(attachment.datas_mongo)
+            f1_hits = re.findall('<NumeroFactura>.*</NumeroFactura>',f1_xml)
+            for f1_hit in f1_hits:
+                f1_number = f1_hit.replace('<NumeroFactura>','').replace('</NumeroFactura>','').strip()
+                if f1_number:
+                    found_f1_codes.append(f1_number)
+        except:
+                pass
 
     found_f1_codes = list(set(found_f1_codes))
-    if len(found_f1_codes) != 1:
+    if len(found_f1_codes) > 1:
         io.warn("detected {} hits searching the NumeroFactura tag in the xmls!! skipping...",len(found_f1_codes))
         many_codes.append(f1line_id)
+        print "id {} detected {} hits in xmls, skipped.".format(f1line_id,len(found_f1_codes))
+        continue
+
+    if len(found_f1_codes) < 1:
+        io.warn("detected {} hits searching the NumeroFactura tag in the xmls!! skipping...",len(found_f1_codes))
+        no_codes.append(f1line_id)
         print "id {} detected {} hits in xmls, skipped.".format(f1line_id,len(found_f1_codes))
         continue
 
@@ -68,9 +78,14 @@ io.info(' - {}',', '.join([str(i) for i in no_attachments]))
 io.info('')
 io.info('With too many codes:')
 io.info(' - {}',', '.join([str(i) for i in many_codes]))
+io.info('')
+io.info('With no codes:')
+io.info(' - {}',', '.join([str(i) for i in no_codes]))
 print "updated:"
 print updated
 print "no attachemnts:"
 print no_attachments
 print "too many codes:"
 print many_codes
+print "no codes:"
+print no_codes
