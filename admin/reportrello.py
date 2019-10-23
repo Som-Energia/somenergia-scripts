d2a56c87a34aef1b3874ab7f06ec6a47185b001a
Oriol Piera <oriol.piera@somenergia.coop>
2019-10-23 11:56:43 +0200

    New paramter "num_ronda" to make the report with all cards from Done list

--- admin/reportrello.py
+++ admin/reportrello.py
@@ -7,6 +7,8 @@ from trello.label import Label
 from trello.board import Board
 import trellovariables
 import re
+import sys
+import argparse
 
 #SETUP
 client = TrelloClient(
@@ -79,27 +81,41 @@ def printTeams(client, teams):
 members = {}
 labels = {}
 
-for card in it_board.get_cards({'fields': 'all'}, "visible"):
-    spend = getCardTime(card.name, TIME_SPEND)
-    estimated = getCardTime(card.name, TIME_ESTIMATED)
- 
-    for label in card.idLabels:
-        if labels.has_key(label):
-            labels[label] = [labels.get(label)[0] + spend , labels.get(label)[1] + estimated]
-        else:
-            labels[label] = [spend,estimated]
+parser = argparse.ArgumentParser(prog='reportrello.py')
+parser.add_argument('-n','--num_ronda', required=True)
+args = parser.parse_args(sys.argv[1:])
+reload(sys)
+sys.setdefaultencoding('utf8')
+num_ronda =  args.num_ronda
 
-    for member in card.idMembers:
-        if not trellovariables.ITMEMBERS.has_key(member) and not trellovariables.ITTEAMS.has_key(member):
-            continue
-        if members.has_key(member):
-            members[member] = [members.get(member)[0] + spend , members.get(member)[1] + estimated]
-        else:
-            members[member] = [spend,estimated]
+for list_data in it_board.all_lists():
+    if num_ronda in list_data.name:
+        #for card in it_board.get_cards({'fields': 'all'}, "visible"):
+        for card in list_data.list_cards():
+            spend = getCardTime(card.name, TIME_SPEND)
+            estimated = getCardTime(card.name, TIME_ESTIMATED)
+            for label in card.idLabels:
+                if labels.has_key(label):
+                    labels[label] = [labels.get(label)[0] + spend , labels.get(label)[1] + estimated]
+                else:
+                    labels[label] = [spend,estimated]
 
-print("======= MEMBERS =========")
-printMembers(client, members)
-print("======= LABELS ==========")
-printLabels(client, labels)
-print("======= TEAMS =========")
-printTeams(client, members)
+            for member in card.idMembers:
+                if not trellovariables.ITMEMBERS.has_key(member) and not trellovariables.ITTEAMS.has_key(member) and not trellovariables.MEMBERS.has_key(member):
+                    p = Member(client, member)
+                    m = p.fetch()
+                    name = m.full_name
+                    print("Warning: Hem d'afegir aquesta persona a la llista de MEMBERS: " + m.full_name + " : " + member)
+                if members.has_key(member):
+                    members[member] = [members.get(member)[0] + spend , members.get(member)[1] + estimated]
+                else:
+                    members[member] = [spend,estimated]
+
+        print("======= MEMBERS =========")
+        printMembers(client, members)
+        print("======= LABELS ==========")
+        printLabels(client, labels)
+        print("======= TEAMS =========")
+        printTeams(client, members)
+
+# vim: et ts=4 sw=4
