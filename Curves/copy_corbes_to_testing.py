fb05aa62fc4fa5fb510ab80f2530eaf03de320f7
Som Energia IT <info@somenergia.coop>
2021-01-20 15:49:58 +0100

    Finish the insert even if there are any errors during de process and better management of the exceptions.

--- Curves/copy_corbes_to_testing.py
+++ Curves/copy_corbes_to_testing.py
@@ -8,7 +8,7 @@ from consolemsg import step, error, warn, fail, success
 import pymongo
 
 
-def get_mongo_data(mongo_db, mongo_collection, curve_type, cups):
+def get_mongo_data(mongo_db, mongo_collection, cups):
     data = dict()
     all_curves_search_query = {'name': {'$regex': '^{}'.format(cups[:20])}}
     fields = {'_id': False}
@@ -21,9 +21,11 @@ def get_mongo_data(mongo_db, mongo_collection, curve_type, cups):
 def set_mongo_data(mongo_db, mongo_collection, curve_type, cups, mongo_data):
     try:
         curves =  mongo_db[mongo_collection].insert(
-            mongo_data)
+            mongo_data, continue_on_error=True)
+    except pymongo.errors.DuplicateKeyError as e:
+        print "Alguns registres de la corba " + curve_type + " ja existeixen."
     except Exception as e:
-        print "La corba " + curve_type + " ja existeix: " + str(e)
+        print "Error no controlat: " + str(e)
         return False
     return True
 
@@ -35,46 +37,42 @@ def main(cups):
     mongo_db_test = mongo_client_test.somenergia
 
     mongo_data_f5d = get_mongo_data(
-        mongo_db=mongo_db_prod, mongo_collection='tg_cchfact',
-        curve_type='f5d', cups = cups
+        mongo_db=mongo_db_prod, mongo_collection='tg_cchfact', cups = cups
     )
     mongo_data_f1 = get_mongo_data(
-        mongo_db=mongo_db_prod, mongo_collection='tg_f1',
-        curve_type='f1', cups = cups
+        mongo_db=mongo_db_prod, mongo_collection='tg_f1', cups = cups
     )
     mongo_data_p5d = get_mongo_data(
-        mongo_db=mongo_db_prod, mongo_collection='tg_cchval',
-        curve_type='p5d', cups = cups
+        mongo_db=mongo_db_prod, mongo_collection='tg_cchval', cups = cups
     )
     mongo_data_p1 = get_mongo_data(
-        mongo_db=mongo_db_prod, mongo_collection='tg_p1',
-        curve_type='p1', cups = cups
+        mongo_db=mongo_db_prod, mongo_collection='tg_p1', cups = cups
     )
 
-    print "Corbes obtingudes f5d: " + str(mongo_data_f5d.count())
-    print "Corbes obtingudes p5d: " + str(mongo_data_p5d.count())
-    print "Corbes obtingudes f1: " + str(mongo_data_f1.count())
-    print "Corbes obtingudes p1: " + str(mongo_data_p1.count())
+    print "Corbes obtingudes F5D: " + str(mongo_data_f5d.count())
+    print "Corbes obtingudes P5D: " + str(mongo_data_p5d.count())
+    print "Corbes obtingudes F1: " + str(mongo_data_f1.count())
+    print "Corbes obtingudes P1: " + str(mongo_data_p1.count())
 
     if mongo_data_f5d.count() > 0:
         result_f5d = set_mongo_data(
             mongo_db=mongo_db_test, mongo_collection='tg_cchfact',
-            curve_type='f5d', cups=cups, mongo_data=mongo_data_f5d
+            curve_type='F5D', cups=cups, mongo_data=mongo_data_f5d
         )
     if mongo_data_p5d.count() > 0:
         result_p5d = set_mongo_data(
             mongo_db=mongo_db_test, mongo_collection='tg_cchval',
-            curve_type='p5d', cups=cups, mongo_data=mongo_data_p5d
+            curve_type='P5D', cups=cups, mongo_data=mongo_data_p5d
         )
     if mongo_data_f1.count() > 0:
         result_f1 = set_mongo_data(
             mongo_db=mongo_db_test, mongo_collection='tg_f1',
-            curve_type='f1', cups=cups, mongo_data=mongo_data_f1
+            curve_type='F1', cups=cups, mongo_data=mongo_data_f1
         )
     if mongo_data_p1.count() > 0:
         result_p1 = set_mongo_data(
             mongo_db=mongo_db_test, mongo_collection='tg_p1',
-            curve_type='p1', cups=cups, mongo_data=mongo_data_p1
+            curve_type='P1', cups=cups, mongo_data=mongo_data_p1
         )
 
     print "Les corbes disponibles s'han pujat a testing"
