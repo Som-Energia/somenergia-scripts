5f2147153c4bf8e12c10872e60debff60d78a665
David García Garzón <david.garcia@somenergia.coop>
2021-01-26 13:12:44 +0100

    extract fillPdfFromCsv, use tempfile

--- accounting/iae/fillInPdf.py
+++ accounting/iae/fillInPdf.py
@@ -5,6 +5,7 @@ from fdfgen import forge_fdf
 import os
 import sys
 from yamlns import namespace as ns
+from tempfile import NamedTemporaryFile
 
 #sys.path.insert(0, os.getcwd())
 filename_prefix_016 = "016"
@@ -33,39 +34,54 @@ def process_csv(filename):
             for row in csv_data[1:]
         ]
 
-def form_fill(pdf_file, filename_prefix, filename_sufix, fields):
-  output_file = '{0}{1}-{2}.pdf'.format(output_folder, filename_prefix, filename_sufix)
-  print("creating {}".format(output_file))
-  fdf = forge_fdf(
-    pdf_form_url="",
-    fdf_data_strings=fields,
-    fdf_data_names=[],
-    fields_hidden=[],
-    fields_readonly=[],
-    checkbox_checked_name="On"
-  )
-  fdf_file = open(tmp_file,"wb")
-  fdf_file.write(fdf)
-  fdf_file.close()
-  cmd = 'pdftk "{0}" fill_form "{1}" output "{2}" dont_ask'.format(pdf_file, tmp_file, output_file)
-  os.system(cmd)
-  os.remove(tmp_file)
+def form_fill(pdf_file, outputdir, filename_prefix, filename_sufix, fields):
+    output_file = '{0}{1}-{2}.pdf'.format(outputdir, filename_prefix, filename_sufix)
+    print("Creating {}".format(output_file))
+    fdf = forge_fdf(
+        pdf_form_url="",
+        fdf_data_strings=fields,
+        fdf_data_names=[],
+        fields_hidden=[],
+        fields_readonly=[],
+        checkbox_checked_name="On"
+    )
+    with NamedTemporaryFile() as fdf_file:
+        fdf_file.write(fdf)
+        fdf_file.flush()
+        cmd = 'pdftk "{0}" fill_form "{1}" output "{2}" dont_ask'.format(pdf_file, fdf_file.name, output_file)
+        os.system(cmd)
 
-os.makedirs(output_folder, exist_ok=True)
+def fillPdfFromCsv(csv, pdf, outputdir, prefix, keyfield, extra=ns()):
+    os.makedirs(outputdir, exist_ok=True)
+    data = process_csv(csv)
+    for row in data:
+        row.update(extra)
+        form_fill(pdf, outputdir, prefix, row[keyfield], row)
+
 print('Generating Forms:')
 print('-----------------------')
-data_016 = process_csv(csv_file_016)
-for fields in data_016:
-  fields.update({
-    'FI DACTIVITAT': True,
-    'SUBJECTE PASSIUSUJETO PASIVO': True,
-    'Empresarial': True,
-    'undefined': True,
-    'EMPRESARIAL': True,
-    })
-  form_fill(pdf_file_016, filename_prefix_016, fields['MUNICIPIMUNICIPIO_4'], fields)
 
-data_703 = process_csv(csv_file_703)
-for fields in data_703:
-  form_fill(pdf_file_703, filename_prefix_703, fields['12 Municipio de la actividad o local indirecto'], fields)
+fillPdfFromCsv(
+    csv=csv_file_016,
+    pdf=pdf_file_016,
+    outputdir=output_folder,
+    keyfield='MUNICIPIMUNICIPIO_4',
+    prefix=filename_prefix_016,
+    extra = {
+        'FI DACTIVITAT': True,
+        'SUBJECTE PASSIUSUJETO PASIVO': True,
+        'Empresarial': True,
+        'undefined': True,
+        'EMPRESARIAL': True,
+        },
+    )
+fillPdfFromCsv(
+    csv=csv_file_703,
+    pdf=pdf_file_703,
+    outputdir=output_folder,
+    keyfield='12 Municipio de la actividad o local indirecto',
+    prefix=filename_prefix_703,
+    )
 
+
+# vim: set ts=4 sw=4 et
