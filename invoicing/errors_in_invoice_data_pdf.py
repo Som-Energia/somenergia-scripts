64fea67d407d47daeb11840c0c185d03f083c4b6
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2021-02-17 15:22:49 +0100

    fix some script errors

--- invoicing/errors_in_invoice_data_pdf.py
+++ invoicing/errors_in_invoice_data_pdf.py
@@ -25,17 +25,48 @@ known_errors = {
 }
 
 
-def search_invoice(invoice_number):
-    step("Cerquem la factura...")
-    fact_ids = fact_obj.search([('number','=',invoice_number)])
+def search_invoice_by_ids(invoice_ids):
+    ret_ids = []
+    invoice_ids = [int(i) for i in invoice_ids.split(',')]
+    for invoice_id in invoice_ids:
+        step("Cerquem la factura...", invoice_id)
+        fact_ids = fact_obj.search([('id', '=', invoice_id)])
+        if len(fact_ids) == 0:
+            warn("Cap factura trobada amb aquest id!!")
+            return None
+        if len(fact_ids) > 1:
+            warn("Multiples factures trobades!! {}", fact_ids)
+            return None
+        ret_ids.append(fact_ids[0])
+        step("Factura amb ID {} existeix", fact_ids[0])
+    return ret_ids
+
+
+def search_invoice_by_id(invoice_id):
+    step("Cerquem la factura... {}", invoice_id)
+    fact_ids = fact_obj.search([('id', '=', invoice_id)])
     if len(fact_ids) == 0:
-        warn("Cap factura trobada!!")
+        warn("Cap factura trobada amb aquest id!!!!")
         return None
     if len(fact_ids) > 1:
-        warn("Multiples factures trobades!! {}",fact_ids)
+        warn("Multiples factures trobades!! {}", fact_ids)
         return None
     fact_id = fact_ids[0]
-    step("Factura amb ID {} trobada",fact_id)
+    step("Factura amb ID {} existeix", fact_id)
+    return fact_id
+
+
+def search_invoice_by_name(invoice_number):
+    step("Cerquem la factura...{}", invoice_number)
+    fact_ids = fact_obj.search([('number', '=', invoice_number)])
+    if len(fact_ids) == 0:
+        warn("Cap factura trobada amb aquest numero!!")
+        return None
+    if len(fact_ids) > 1:
+        warn("Multiples factures trobades!! {}", fact_ids)
+        return None
+    fact_id = fact_ids[0]
+    step("Factura amb ID {} trobada", fact_id)
     return fact_id
 
 
@@ -49,8 +80,14 @@ def load_invoice_data(fact_id):
     except Exception as e:
         error = True
         step("Error localitzat ...")
-        data = data_yaml.replace("\n",'\n')
+        data = data_yaml.replace("\n", '\n')
     return error, data
+
+def print_invoice_data(fact_id):
+    f = fact_obj.browse(fact_id)
+    warn("Codi factura .... {}", f.number)
+    warn("Id factura ...... {}", fact_id)
+    warn("Polissa ......... {}", f.polissa_id.name)
 
 
 def search_known_errors(res, fact_id):
@@ -59,24 +96,26 @@ def search_known_errors(res, fact_id):
         if k in res:
             errors.append(known_errors[k])
     if errors:
-        success("S'ha trobat {} possible(s) error(s):",len(errors))
+        success("S'ha trobat {} possible(s) error(s):", len(errors))
         for error in errors:
-            success(error)
+            success("POSSIBLE ERROR >> {}", error)
     else:
-        f = fact_obj.browse(fact_id)
         warn("Error no reconegut, fes captura i obre incidència!!")
-        warn("Codi factura .... {}", f.number)
-        warn("Id factura ...... {}", fact_id)
-        warn("Polissa ......... {}", f.polissa_id.name)
-
     step("Traça interna de l'error:")
     step(res)
     return errors
 
 
-def main(fact_name):
-    fact_id = search_invoice(fact_name)
-    if fact_id:
+def main(invoice_name, invoice_id, invoice_ids):
+    fact_ids = []
+    if invoice_name:
+        fact_ids.append(search_invoice_by_name(invoice_name))
+    if invoice_id:
+        fact_ids.append(search_invoice_by_id(invoice_id))
+    if invoice_ids:
+        fact_ids.extend(search_invoice_by_ids(invoice_ids))
+    for fact_id in fact_ids:
+        print_invoice_data(fact_id)
         error, res = load_invoice_data(fact_id)
         if error:
             errors = search_known_errors(res, fact_id)
@@ -84,22 +123,49 @@ def main(fact_name):
             success("La factura en pdf no te problemes a nivell de dades!")
 
 
+def only_one(a, b, c):
+    n = 0
+    if bool(a):
+        n += 1
+    if bool(b):
+        n += 1
+    if bool(c):
+        n += 1
+    return n == 1
+
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser(
-            description=''
+            description='Comprovador del generador de dades de factures en pdf'
     )
 
     parser.add_argument(
         '--invoice_name',
-        dest='inv_name',
-        required=True,
-        help="Nombre de la factura"
+        dest='i_name',
+        help="Nom de la factura"
+    )
+
+    parser.add_argument(
+        '--invoice_id',
+        type=int,
+        dest='i_id',
+        help="Id de la factura"
+    )
+
+    parser.add_argument(
+        '--invoice_ids',
+        dest='i_ids',
+        help="llista d'Id de les factures"
     )
 
     args = parser.parse_args()
 
+    if not only_one(args.i_name, args.i_id, args.i_ids):
+        parser.print_help()
+        sys.exit()
+
     try:
-        main(args.inv_name)
+        main(args.i_name, args.i_id, args.i_ids)
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
         error("El proces no ha finalitzat correctament: {}", str(e))
