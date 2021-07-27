19a89586903d1fad90a6cb09231e1e894623e37d
David Suárez <davidsuarez92@gmail.com>
2021-07-27 11:31:35 +0200

    new format expected for contract endpoint

--- admin/contractacio_massiva/contractacio_massiva.py
+++ admin/contractacio_massiva/contractacio_massiva.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import argparse
 import csv
 
+import json
 import requests
 import contextlib
 import sys, traceback
+sys.path.append('/home/david/Documents/somenergia-scripts')
 from consolemsg import step, success, warn, error
 from ooop_wst import OOOP_WST
 from yamlns import namespace as ns
@@ -13,6 +15,39 @@ from yamlns import namespace as ns
 from comer.canvi_titus.models import get_or_create_partner_address
 from comer.canvi_titus.utils import get_last_contract_on_cups
 import configdb
+
+def _custom_casting(data):
+    power_items = ['power_p3','power_p4','power_p5','power_p6']
+    data['contract_owner']['city_id'] = int(data['contract_owner']['city_id'])
+    data['contract_owner']['state_id'] = int(data['contract_owner']['state_id'])
+    data['cups_city_id'] = int(data['cups_city_id'])
+    data['cups_state_id'] = int(data['cups_state_id'])
+    if data.get('owner_is_payer').lower() == 'false':
+        data['contract_payer']['city_id'] = int(data['contract_payer']['city_id'])
+        data['contract_payer']['state_id'] = int(data['contract_payer']['state_id'])
+    for power in power_items:
+        if data[power] == '0':
+            del data[power]
+
+    return data
+
+def _decode(o):
+    if isinstance(o, str) or isinstance(o, unicode):
+        try:
+            if o.lower() == 'true':
+                return True
+            if o.lower() == 'false':
+                return False
+            return o
+
+        except ValueError:
+            return o
+    elif isinstance(o, dict):
+        return {k: _decode(v) for k, v in o.items() if v != ''}
+    elif isinstance(o, list):
+        return [_decode(v) for v in o]
+    else:
+        return o
 
 @contextlib.contextmanager
 def transaction(O):
@@ -30,7 +65,8 @@ def transaction(O):
 
 def add_contract(uri, data):
     try:
-        response = requests.post(uri, data=data)
+        json = _decode(_custom_casting(data))
+        response = requests.post(uri, json=json)
         response.raise_for_status()
         keys = [key for key in  response.json()['data']]
         if 'invalid_fields' in keys:
@@ -53,7 +89,6 @@ def add_contract(uri, data):
     else:
         return response.status_code, response.reason, response.text
 
-
 def read_contracts_data_csv(csv_file):
     with open(csv_file, 'rb') as f:
         reader = csv.reader(f, delimiter=';')
@@ -70,81 +105,44 @@ def read_contracts_data_csv(csv_file):
         csv_content = [(dict(zip(header, row))) for row in reader if row[0]]
 
         for row in csv_content:
-            fields = [item for item in row if 'contract_owner' in item]
+            aniuats = [item for item in row if 'contract_owner' in item]
             row['contract_owner'] = {}
 
-            for item in fields:
-                row['contract_owner'][item] = row[item]
+            for item in aniuats:
+                simple_item = item.split('contract_owner_')[1]
+                row['contract_owner'][simple_item] = row[item]
+                del row[item]
+
+            aniuats = [item for item in row if 'contract_payer' in item]
+            row['contract_payer'] = {}
+
+            for item in aniuats:
+                simple_item = item.split('contract_payer_')[1]
+                row['contract_payer'][simple_item] = row[item]
                 del row[item]
 
     return csv_content
 
-
-def create_contact_address(O, petition):
-    contract_id = None
-    try:
-        contract_id = get_last_contract_on_cups(O, petition.cups)
-    except:
-        error = "This cups did not have any previous contract in somenergia"
-        raise Exception(error)
-
-    partner_id, _ = O.GiscedataPolissa.read(contract_id, ['titular'])['titular']
-
-    country_id = O.ResCountry.search([('code','like','ES')])[0]
-    new_partner_address_id, created  = get_or_create_partner_address(
-        O,
-        partner_id=partner_id,
-        street='Adreça electronica - ' + petition.compte_email,
-        postal_code=petition.titular_cp,
-        city_id=int(petition.titular_municipi),
-        state_id=petition.titular_provincia,
-        country_id=country_id,
-        email=petition.compte_email,
-        phone=''
-    )
-
-    values={
-        'notificacio': 'titular',
-        'direccio_notificacio': new_partner_address_id,
-        'titular': partner_id
-    }
-    O.GiscedataPolissa.write(
-        contract_id, values
-    )
-    return O.GiscedataPolissa.read(contract_id, ['name'])['name']
 
 def crea_contractes(uri, filename):
     O = OOOP_WST(**configdb.ooop)
     contract_petitions = read_contracts_data_csv(filename)
 
     for petition in contract_petitions:
-        if getattr(configdb, 'TESTING', False):
-            petition.id_soci = configdb.personaldata['nsoci']
-            petition.dni = configdb.personaldata['nif']
-            petition.soci_titular = 0
         msg = "Creating contract for vat {}, soci {}, CUPS {}"
-        step(msg, petition.dni, petition.id_soci, petition.cups)
+        step(msg, petition['contract_owner']['vat'], petition['member_number'], petition['cups'])
         try:
             status, reason, text = add_contract(uri, petition)
             # comprovació manual ja que sempre retorna 200 el webforms
-            if len(O.GiscedataPolissa.search([('cups', '=', petition.cups)])) == 0:
+            if len(O.GiscedataPolissa.search([('cups', '=', petition['cups'])])) == 0:
                 raise requests.exceptions.HTTPError("Error en resposta del webforms")
         except requests.exceptions.HTTPError as e:
             msg = "I couldn\'t create a new contract for cups {}, reason {}"
             if 'cups exist' in e.message:
-                warn(msg, petition.cups, e)
+                warn(msg, petition['cups'], e)
             else:
-                error(msg, petition.cups, e)
-        else:
-            try:
-                with transaction(O) as t:
-                    contracte = create_contact_address(O, petition)
-            except Exception as e:
-                traceback.print_exc(file=sys.stdout)
-                msg = "An uncontroled exception ocurred, reason: {}"
-                error(msg, str(e))
-            else:
-                success("S'ha creat un nou contracte amb numero: {} pel CUPS {}".format(contracte, petition.cups))
+                error(msg, petition['cups'], e)
+        success("S'ha creat un nou contracte pel CUPS {}".format(petition['cups']))
 
 def main(csv_file):
 
@@ -152,7 +150,7 @@ def main(csv_file):
 
     if not uri:
         raise Exception("No se ha definido a qué API ataca el script")
-    
+
     crea_contractes(uri, csv_file)
 
 
@@ -179,3 +177,5 @@ if __name__ == '__main__':
         error("El proceso no ha finalizado correctamente: {}", str(e))
     else:
         success("Script finalizado")
+
+# vim: et ts=4 sw=4
