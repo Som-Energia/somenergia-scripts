02d70c976be5dee29303717308f945ac36f4c9a5
Francisco Jose Cubero Sanchez <francisco.cubero@somenergia.coop>
2022-01-05 11:50:31 +0100

    script now searches and detects 3.X too

--- invoicing/encallats_volta_comptador.py
+++ invoicing/encallats_volta_comptador.py
@@ -43,15 +43,19 @@ def main():
     id_lot_obert = lot_obj.search([('state','=','obert')])[0]
     success("Lot actualment en us id {}", id_lot_obert)
     
-    clot_ids = clot_obj.search([('lot_id', '=', id_lot_obert), ('tarifaATR','like', '2.%'),('status', 'like', '%[V001]%')])
-    success("Factures 2.X amb V001 al lot actual trobades: {}", len(clot_ids))
+    clot_2_ids = clot_obj.search([('lot_id', '=', id_lot_obert), ('tarifaATR','like', '2.%'),('status', 'like', '%[V001]%')])
+    success("Factures 2.X amb V001 al lot actual trobades: {}", len(clot_2_ids))
+    clot_3_ids = clot_obj.search([('lot_id', '=', id_lot_obert), ('tarifaATR','like', '3.%'),('status', 'like', '%[V001]%')])
+    success("Factures 3.X amb V001 al lot actual trobades: {}", len(clot_3_ids))
 
-    csv_writer.writerow(['Nom polissa', 'Data ultima lectura facturada'])
+    clot_ids = clot_2_ids + clot_3_ids
+
+    csv_writer.writerow(['Nom polissa', 'Data ultima lectura facturada', 'Tarifa'])
     for clot_id in tqdm(clot_ids):
         cl = clot_obj.browse(clot_id)
         if encallat_volta_comptador(cl.polissa_id.cups.id, cl.polissa_id.data_ultima_lectura):
-            csv_writer.writerow([cl.polissa_id.name, cl.polissa_id.data_ultima_lectura])
-            step("trobada polissa {} amb F1 tipus R amb data final coincident amb data lectura final {}", cl.polissa_id.name, cl.polissa_id.data_ultima_lectura)
+            csv_writer.writerow([cl.polissa_id.name, cl.polissa_id.data_ultima_lectura, cl.polissa_id.tarifa.name])
+            step("trobada polissa {} - {} amb F1 tipus R amb data final coincident amb data lectura final {}", cl.polissa_id.name, cl.polissa_id.tarifa.name, cl.polissa_id.data_ultima_lectura)
 
 if __name__ == '__main__':
     main()
