dbe54322f4ecb848d6f7b7be5e628ec712f57958
David Suárez <davidsuarez92@gmail.com>
2022-01-20 15:14:22 +0100

    get first search result

--- Informes/crida_balanc_per_correu.py
+++ Informes/crida_balanc_per_correu.py
@@ -8,15 +8,16 @@ from consolemsg import step, success, error, warn, color, printStdError
 def main():
     step("Connectant a l'erp")
     O = Client(**configdb.erppeek)
-    success("Connectat")
+    success("Connectat a: {}".format(O))
+    success("Usuaria: {}".format(O.user))
 
-    fiscal_year_id = O.model('account.fiscalyear').search([('name', 'ilike', '2021')])
+    fiscal_year_id = O.model('account.fiscalyear').search([('name', 'ilike', '2021')])[0]
     fiscal_year = O.model('account.fiscalyear').browse(fiscal_year_id)
 
     step("Demanem el balaç per l'any {}".format(fiscal_year.name))
 
     O.wizard('account.balance.full.report',
-             {'form': {'company_id': 1, 'account_list': [[6, 0, []]], 'fiscalyear': fiscal_year.id,
+             {'form': {'company_id': 1, 'account_list': [[6, 0, []]], 'fiscalyear': fiscal_year_id,
                        'display_account': 'bal_mouvement', 'state': 'none', 'periods': [(6, 0, [])],
                        'all_accounts': True, 'context': {}, 'display_account_level': 5
                        }
