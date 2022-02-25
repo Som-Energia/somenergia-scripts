bdbae77716e3735ded0f5c72fd6e9606d497755c
Oriol Piera <oriol.piera@somenergia.coop>
2022-02-25 17:33:39 +0100

    Add sleep after import, and restore previous states

--- Importacions_F1_Q1/reimport_sense_motiu_rebuig.py
+++ Importacions_F1_Q1/reimport_sense_motiu_rebuig.py
@@ -1,12 +1,16 @@
-# Script per reimportar pasos de rebuig de casos ATR, on no s'ha informat correctament motiu de rebuig (incidència al importar amb el wizard Gestió ATR > Importació casos ATR
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Script per reimportar pasos de rebuig de casos ATR, on no sha informat correctament motiu de rebuig (incidència al importar amb el wizard Gestió ATR > Importació casos ATR
 
 from erppeek import Client
 from base64 import b64decode
 import tempfile
 from os.path import basename
-from tqdm import tqdm
+import dbconfig
+from time import sleep
 
-O = Client(**dbconfig)
+O = Client(**dbconfig.erppeek)
 sw_obj = O.GiscedataSwitching
 
 models = ['giscedata.switching.a3.02','giscedata.switching.a3.04','giscedata.switching.c1.02','giscedata.switching.c2.02']
@@ -19,9 +23,14 @@ for model in models:
     print "Comencem el model " + model
     passos_ids = model_obj.search([('rebuig','=',True),('rebuig_ids','=',[]), ('data_rebuig', '>', '2021-09-01')])
     print "Pas 1: Hi ha " + str(len(passos_ids)) + " a reimportar."
-    for pas_id in tqdm(passos_ids):
+    for pas_id in passos_ids:
         pas = model_obj.browse(pas_id)
         cas = sw_obj.browse(pas.sw_id.id)
+        # En guardem l'estat
+        old_states = {}
+        old_states['notificacio_pendent'] = cas.notificacio_pendent
+        old_states['enviament_pendent'] =  cas.enviament_pendent
+        old_states['validacio_pendent'] =  cas.validacio_pendent
         sw_id = cas.id
         ia_ids = O.IrAttachment.search([('res_model','=','giscedata.switching'),('res_id','=',sw_id)])
         print "Pas 2: Trobem " + str(len(ia_ids)) + " adjunts."
@@ -32,12 +41,12 @@ for model in models:
                 te_adjunt = True
                 break
             print "Pas 3: No hem trobat l'adjunt per aquest cas, per tant passem al següent cas"
-            
+
         if not te_adjunt:
             continue
-    
+
         wiz_obj = O.model('giscedata.switching.wizard')
-        
+
         with tempfile.NamedTemporaryFile() as aux:
             temp_name = aux.name
             try:
@@ -51,14 +60,17 @@ for model in models:
                         'name': basename(aux.name),
                     }, context={'active_ids': [sw_id], 'active_id': sw_id}
                 )
-                ia.unlink()
                 pas.unlink()
                 wiz_obj.action_importar_xml([wiz.id])
+                sleep(10)
+                print wiz.info
                 print "Pas 5: XML reimportat correctament amb codi solicitud " + cas.codi_sollicitud
-
                 nou_pas = model_obj.browse(cas.step_ids.pas_id[-1].id)
-                nou_pas.write({'notificacio_pendent': False})
-                print "Pas 6: Notificació pendent desmarcat"
+                nou_pas.write(old_states)
+                cas.case_close()
+                print "Pas 6: Estats del Cas ATR restaurats"
                 aux.name = temp_name
-            except:
+                ia.unlink()
+            except Exception, e:
+                print e
                 print "Error: No s'ha trobat el fitxer a Mongo"
