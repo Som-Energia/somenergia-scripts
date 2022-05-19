7de6c5ebee63b352beb4f4fe5c06bd683879f082
Oriol Piera <oriol.piera@somenergia.coop>
2022-05-19 11:14:24 +0200

    Suppport from_date in get_curve_info.py script "curves molonguis"

--- Curves/get_curves_info.py
+++ Curves/get_curves_info.py
@@ -13,6 +13,7 @@ from emili import sendMail
 import pymongo
 from datetime import datetime, timedelta, date, time
 from pymongo import DESCENDING
+import argparse
 
 from tqdm import tqdm
 from time import sleep
@@ -63,12 +64,15 @@ def get_count_and_date_firmeza(mongo_db, mongo_collection, cups, firmeza, date):
         data_ultima = cursor[0]['datetime']
     return count_curves, data_ultima
 
-def get_mongo_data(mongo_db, mongo_collection, curve_type, cups):
+def get_mongo_data(mongo_db, mongo_collection, curve_type, cups, from_date=None):
     data = dict()
-    last_year = datetime.combine(date.today(), time()) - timedelta(days=365)
+    if from_date:
+        from_date = datetime.strptime(from_date, "%Y-%m-%d")
+    else:
+        from_date = datetime.combine(date.today(), time()) - timedelta(days=365)
     all_curves_search_query = {
             'name': {'$regex': '^{}'.format(cups[:20])},
-            'datetime': {"$gte": last_year},
+            'datetime': {"$gte": from_date},
     }
     if mongo_collection == 'tg_p1':
         all_curves_search_query.update({"type": "p"})
@@ -99,7 +103,7 @@ def get_mongo_data(mongo_db, mongo_collection, curve_type, cups):
                 mongo_collection=mongo_collection,
                 firmeza = False,
                 cups=cups,
-                date=last_year,
+                date=from_date,
         )
         data['corbes_firmes_{}'.format(curve_type)] = number_curves - data['corbes_no_firmes_{}'.format(curve_type)]
 
@@ -134,7 +138,7 @@ def get_mongo_fields():
     return mongo_fields
 
 
-def main():
+def main(from_date):
     erp_client = Client(**configdb.erppeek)
     mongo_client = pymongo.MongoClient(configdb.mongodb)
     mongo_db = mongo_client.somenergia
@@ -176,18 +180,22 @@ def main():
                 mongo_data_f5d = get_mongo_data(
                     mongo_db=mongo_db, mongo_collection='tg_cchfact',
                     curve_type='f5d', cups=polissa['cups'][1],
+                    from_date=from_date,
                 )
                 mongo_data_f1 = get_mongo_data(
                     mongo_db=mongo_db, mongo_collection='tg_f1',
                     curve_type='f1', cups=polissa['cups'][1],
+                    from_date=from_date,
                 )
                 mongo_data_p5d = get_mongo_data(
                     mongo_db=mongo_db, mongo_collection='tg_cchval',
                     curve_type='p5d', cups=polissa['cups'][1],
+                    from_date=from_date,
                 )
                 mongo_data_p1 = get_mongo_data(
                     mongo_db=mongo_db, mongo_collection='tg_p1',
                     curve_type='p1', cups=polissa['cups'][1],
+                    from_date=from_date,
                 )
 
                 cleared_polissa.update(mongo_data_f5d)
@@ -206,4 +214,16 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+            description='Script curves molonguis'
+    )
+
+    parser.add_argument(
+        '--from_date',
+        dest='from_date',
+        required=False,
+        help="Introdueix la data d'inici del perídode que cal decarregar 'YYYY-mm-dd'. Si no es posa res, agafa els últims 365 dies."
+    )
+    args = parser.parse_args()
+
+    main(args.from_date)
