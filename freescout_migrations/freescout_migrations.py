62427c3be955ca746a4c9a5b19002764b48b057e
benjami <benjami.ramos@somenergia.coop>
2024-06-18 07:37:43 +0200

    FreeScout migration improvements
    * Fixed who's write note or reply conversations
    * Added conversation tags
    
    Co-authored-by: Joana <joana.figueira@somenergia.coop>

--- freescout_migrations/freescout_migrations.py
+++ freescout_migrations/freescout_migrations.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from configdb import URL, SECRET_KEY, MAILBOX_DESTINATION, HS_TOKEN
+from configdb import URL, SECRET_KEY, MAILBOX_DESTINATION, HS_TOKEN, USER_MAPPING
 
 HS_HEADERS = {
     'Authorization': f'Bearer {HS_TOKEN}',
@@ -14,68 +14,82 @@ FS_HEADERS = {
     "Accept": "application/json",
 }
 
+POST_CONVERSATION_URL = "{}{}".format(URL, 'conversations')
+ADD_TAGS_URL = "{}{}".format(URL, 'conversations/{}/tags')
 
 def create_conversation(hs_conversation):
-    request_url = "{}{}".format(URL, 'conversations')
+    try:
 
-    threads = []
-    for th in hs_conversation['_embedded']['threads']:
-        thread = {
-            "type": th['type'],
-            "user": 1,  # TODO: Maping
+        threads = []
+        for th in hs_conversation['_embedded']['threads']:
+            thread = {
+                "type": th['type'],
+                "imported": True,
+                "createdAt": th['createdAt']
+            }
+            if th['type'] != 'customer':
+                thread["user"] = USER_MAPPING[th['createdBy']['id']]
+            if 'body' in th:
+                thread['text'] = th['body']
+            if th['type'] == 'customer':
+                thread['cc'] = th['cc']
+                thread['bcc'] = th['bcc']
+                thread['customer'] = {'email': th['createdBy']['email']}
+                thread['createdAt'] = th['createdAt']
+            thread['attachments'] = []
+            for attach in th['_embedded']['attachments']:
+                attachment_ref = attach['_links']['data']['href']
+                attachment_response = requests.get(url=attachment_ref, headers=HS_HEADERS)
+                if attachment_response.status_code == 200:
+                    thread['attachments'].append({
+                        "fileName": attach['filename'],
+                        "mimeType": attach['mimeType'],
+                        "data": attachment_response.json()['data']
+                    })
+                else:
+                    print("Problem getting attachment")
+
+            threads.append(thread)
+
+        body = {
+            "type": hs_conversation['type'],
+            "mailboxId": MAILBOX_DESTINATION,
+            "subject": hs_conversation['subject'],
+            "customer": {
+                "email": hs_conversation['primaryCustomer'].get('email', ""),
+                "firstName": hs_conversation['primaryCustomer'].get('first', ""),
+                "lastName": hs_conversation['primaryCustomer'].get('last', ""),
+            },
+            "threads": threads,
             "imported": True,
-            "createdAt": th['createdAt']
+            "status": hs_conversation['status'],
+            "createdAt": hs_conversation['createdAt']
         }
-        if 'body' in th:
-            thread['text'] = th['body']
-        if th['type'] == 'customer':
-            thread['cc'] = th['cc']
-            thread['bcc'] = th['bcc']
-            thread['customer'] = {'email': th['createdBy']['email']}
-            thread['createdAt'] = th['createdAt']
-        thread['attachments'] = []
-        for attach in th['_embedded']['attachments']:
-            attachment_ref = attach['_links']['data']['href']
-            attachment_response = requests.get(url=attachment_ref, headers=HS_HEADERS)
-            if attachment_response.status_code == 200:
-                thread['attachments'].append({
-                    "fileName": attach['filename'],
-                    "mimeType": attach['mimeType'],
-                    "data": attachment_response.json()['data']
-                })
-            else:
-                print("Problem getting attachment")
+        if 'assignee' in hs_conversation:
+            body['assignTo'] = USER_MAPPING[hs_conversation['assignee']['id']]
+        if hs_conversation['status'] == 'closed':
+            body['closedAt'] = hs_conversation['closedAt']
+        fs_tags = []
+        if 'tags' in hs_conversation:
+            for tag in hs_conversation['tags']:
+                fs_tags.append(tag['tag'])
 
-        threads.append(thread)
+    except:
+        print("No he pogut gestionar:", hs_conversation.get('subject'), hs_conversation.get('createdAt'))
 
-    body = {
-        "type": hs_conversation['type'],
-        "mailboxId": MAILBOX_DESTINATION,
-        "subject": hs_conversation['subject'],
-        "customer": {
-            "email": hs_conversation['primaryCustomer']['email'],
-            "firstName": hs_conversation['primaryCustomer']['first'],
-            "lastName": hs_conversation['primaryCustomer']['last']
-        },
-        "threads": threads,
-        "imported": True,
-        "assignTo": 1,  # TODO: Maping
-        "status": hs_conversation['status'],
-        "createdAt": hs_conversation['createdAt']
-    }
+    response = requests.post(POST_CONVERSATION_URL, json=body, headers=FS_HEADERS)
 
-    if hs_conversation['status'] == 'closed':
-        body['closedAt'] = hs_conversation['closedAt']
-
-    r = requests.post(request_url, json=body, headers=FS_HEADERS)
-
-    print(r.status_code)
-
+    if response.status_code != 201:
+        print(response.status_code, hs_conversation['subject'])
+    elif fs_tags:
+        response_tags = requests.put(ADD_TAGS_URL.format(response.json()['id']), json={'tags': fs_tags}, headers=FS_HEADERS)
+        if response_tags.status_code != 204:
+            print("Tag Error: ", response_tags.status_code, hs_conversation['subject'])
 
 if __name__ == '__main__':
     mailbox = 249427  # Technocuca, mirar https://api.helpscout.net/v2/mailboxes
-    start_date = '2023-01-01T00:00:00Z'
-    end_date = '2023-05-31T23:59:59Z'
+    start_date = '2024-03-04T00:00:00Z'
+    end_date = '2024-03-04T23:59:59Z'
 
     url = f'https://api.helpscout.net/v2/conversations?mailbox={mailbox}&embed=threads&query=(createdAt:[{start_date} TO {end_date}])&status=all'
 
