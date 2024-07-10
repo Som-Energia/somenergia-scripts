a2ea5964471b41d11316c8bc6ede85954216d2ad
benjami <benjami.ramos@somenergia.coop>
2024-07-10 14:08:43 +0200

    Added tqdm (te quiero de muerte) 💚

--- freescout_migrations/freescout_migrations.py
+++ freescout_migrations/freescout_migrations.py
@@ -3,6 +3,7 @@
 import requests
 from configdb import URL, SECRET_KEY, HS_TOKEN
 from create_conversation import create_conversation
+from tqdm import tqdm
 
 
 HS_HEADERS = {
@@ -20,13 +21,20 @@ POST_CONVERSATION_URL = "{}{}".format(URL, 'conversations')
 ADD_TAGS_URL = "{}{}".format(URL, 'conversations/{}/tags')
 
 
+def get_tqdm(url):
+    response = requests.get(url, headers=HS_HEADERS)
+    page_info = response.json().get('page', {})
+    return tqdm(total=page_info.get('totalElements', 0)), page_info.get('size', 1)
+
+
 if __name__ == '__main__':
-    mailbox = 187923  # Technocuca, mirar https://api.helpscout.net/v2/mailboxes
+    mailbox = 187926  # Mirar https://api.helpscout.net/v2/mailboxes
     start_date = '2010-01-01T00:00:00Z'
-    end_date = '2025-03-04T23:59:59Z'
+    end_date = '2025-01-01T23:59:59Z'
 
     url = f'https://api.helpscout.net/v2/conversations?mailbox={mailbox}&embed=threads&query=(createdAt:[{start_date} TO {end_date}] AND tag:(NOT "nagios" AND NOT "fail2ban" AND NOT "backups"))&status=all'
 
+    pbar, page_size = get_tqdm(url)
     next_url = url
     while next_url:
         response = requests.get(next_url, headers=HS_HEADERS)
@@ -36,8 +44,9 @@ if __name__ == '__main__':
 
             for item in data['_embedded']['conversations']:
                 create_conversation(hs_conversation=item)
-
+            pbar.update(page_size)
             next_url = data['_links'].get('next', dict()).get('href')
         else:
             print(f'Error in request: {response.status_code}')
             print(response.text)
+    pbar.close()
