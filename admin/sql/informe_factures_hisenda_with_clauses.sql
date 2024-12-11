601903f258266d6a3fe7972d7c296173a2c0be06
Oriol Piera <oriol.piera@somenergia.coop>
2024-12-11 10:56:41 +0100

    Add sign to reactiva & lloguers. Force all invoices to have IVA

--- admin/sql/informe_factures_hisenda_with_clauses.sql
+++ admin/sql/informe_factures_hisenda_with_clauses.sql
@@ -51,7 +51,6 @@ ives AS (
   WHERE tax.name LIKE 'IVA' OR tax.name LIKE 'IVA%%'
   GROUP BY i.id
 ),
-
 no_energy_invoices_with_vat AS
 (
   SELECT il.invoice_id AS inv_id, SUM(il.price_subtotal) AS other_lines
@@ -63,7 +62,6 @@ no_energy_invoices_with_vat AS
     and fl.id is null
   GROUP BY il.invoice_id
 ),
-
 no_energy_invoices_no_vat AS
 (
   SELECT il.invoice_id AS inv_id, SUM(il.price_subtotal) AS other_lines
@@ -75,7 +73,6 @@ no_energy_invoices_no_vat AS
     and fl.id is null
   GROUP BY il.invoice_id
 ),
-
 otros_cobros AS
 (
   SELECT il.invoice_id AS inv_id, SUM(il.price_subtotal) AS cobros
@@ -84,7 +81,6 @@ otros_cobros AS
   WHERE fl.tipus = 'cobrament'
   GROUP BY il.invoice_id
 ),
-
 iv_21 AS
 (
   SELECT i.id, SUM(tax.amount) AS amount, sum(tax.base) AS base
@@ -93,7 +89,6 @@ iv_21 AS
   WHERE tax.name LIKE 'IVA' OR tax.name LIKE 'IVA%%'
   GROUP BY i.id
 )
-
 
 SELECT
     i.number AS factura,
@@ -104,25 +99,26 @@ SELECT
     f.data_inici AS fecha_inicio,
     f.data_final AS fecha_final,
     CASE WHEN state.code IN ('01', '20', '31', '48') THEN state.name ELSE 'resto' END AS provincia,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(f.total_potencia, 0.0) ELSE COALESCE(f.total_potencia, 0.0) END AS importe_potencia_sin_descuento,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(energia_custom.total_energia, 0.0) ELSE COALESCE(energia_custom.total_energia, 0.0) END AS importe_energia_sin_descuento,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(generacio_custom.total_energia, 0.0) ELSE COALESCE(generacio_custom.total_energia, 0.0) END AS importe_generacio,
-    f.total_reactiva AS importe_reactiva,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(excesos.suma, 0.0) ELSE COALESCE(excesos.suma, 0.0) END AS importe_exceso_potencia,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(no_iva_concepts.base_costs, 0.0) ELSE COALESCE(no_iva_concepts.base_costs, 0.0) END AS otros_sin_iva,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(iva_concepts.base_costs, 0.0) ELSE COALESCE(iva_concepts.base_costs, 0.0) END AS otros_con_iva,
-    f.total_lloguers AS importe_alquiler,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(no_energy_invoices_with_vat.other_lines, 0.0) ELSE COALESCE(no_energy_invoices_with_vat.other_lines, 0.0) END AS otros_con_iva,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(no_energy_invoices_no_vat.other_lines, 0.0) ELSE COALESCE(no_energy_invoices_no_vat.other_lines, 0.0) END AS otros_con_iva,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN -COALESCE(otros_cobros.cobros, 0.0) ELSE COALESCE(otros_cobros.cobros,0.0) END AS otros_cobros,
-    CASE WHEN i.type LIKE '%%refund' THEN -COALESCE(it_iese.base, 0.0) ELSE COALESCE(it_iese.base, 0.0) END AS base_iese,
-    CASE WHEN i.type LIKE '%%refund' THEN -COALESCE(it_iese.amount, 0.0) ELSE COALESCE(it_iese.amount, 0.0) END AS base_iese,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN COALESCE(-iv_21.base, NULL) ELSE COALESCE(iv_21.base, NULL) END AS base_iva,
-    CASE WHEN i.type IN ('out_refund', 'in_refund') THEN COALESCE(-iv_21.amount, NULL) ELSE COALESCE(iv_21.amount, NULL) END AS cuota_iva,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(f.total_potencia, 0.0) ELSE COALESCE(f.total_potencia, 0.0) END AS importe_potencia_sin_descuento,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(energia_custom.total_energia, 0.0) ELSE COALESCE(energia_custom.total_energia, 0.0) END AS importe_energia_sin_descuento,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(generacio_custom.total_energia, 0.0) ELSE COALESCE(generacio_custom.total_energia, 0.0) END AS importe_generacio,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(f.total_reactiva, 0.0) ELSE COALESCE(f.total_reactiva, 0.0) END AS importe_reactiva,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(excesos.suma, 0.0) ELSE COALESCE(excesos.suma, 0.0) END AS importe_exceso_potencia,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(no_iva_concepts.base_costs, 0.0) ELSE COALESCE(no_iva_concepts.base_costs, 0.0) END AS otros_sin_iva,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(iva_concepts.base_costs, 0.0) ELSE COALESCE(iva_concepts.base_costs, 0.0) END AS otros_con_iva,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(f.total_lloguers, 0.0) ELSE COALESCE(f.total_lloguers, 0.0) END AS importe_alquiler,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(no_energy_invoices_with_vat.other_lines, 0.0) ELSE COALESCE(no_energy_invoices_with_vat.other_lines, 0.0) END AS otros_con_iva,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(no_energy_invoices_no_vat.other_lines, 0.0) ELSE COALESCE(no_energy_invoices_no_vat.other_lines, 0.0) END AS otros_con_iva,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(otros_cobros.cobros, 0.0) ELSE COALESCE(otros_cobros.cobros,0.0) END AS otros_cobros,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(it_iese.base, 0.0) ELSE COALESCE(it_iese.base, 0.0) END AS base_iese,
+    CASE WHEN i.type = 'out_refund' THEN -COALESCE(it_iese.amount, 0.0) ELSE COALESCE(it_iese.amount, 0.0) END AS base_iese,
+    CASE WHEN i.type = 'out_refund' THEN COALESCE(-iv_21.base, NULL) ELSE COALESCE(iv_21.base, NULL) END AS base_iva,
+    CASE WHEN i.type = 'out_refund' THEN COALESCE(-iv_21.amount, NULL) ELSE COALESCE(iv_21.amount, NULL) END AS cuota_iva,
     CASE WHEN ives.total_taxes > 1 THEN 'varios' ELSE ives.max_tax_name END AS tipo_iva,
-    CASE WHEN i.type LIKE '%%refund' THEN -i.amount_total ELSE i.amount_total END AS total_Factura
+    CASE WHEN i.type = 'out_refund' THEN -i.amount_total ELSE i.amount_total END AS total_Factura
 FROM account_invoice i
 LEFT JOIN giscedata_facturacio_factura f ON i.id = f.invoice_id
+INNER JOIN iv_21 ON iv_21.id = i.id
 LEFT JOIN giscedata_cups_ps c ON f.cups_id = c.id
 LEFT JOIN res_partner tit ON tit.id = i.partner_id -- Afegit l'enllaç correcte
 LEFT JOIN res_municipi mun ON mun.id = c.id_municipi
@@ -133,7 +129,6 @@ LEFT JOIN excesos ON excesos.factura_id = f.id
 LEFT JOIN no_iva_concepts ON no_iva_concepts.inv_id = i.id
 LEFT JOIN iva_concepts ON iva_concepts.inv_id = i.id
 LEFT JOIN account_invoice_tax it_iese ON (it_iese.invoice_id=i.id AND it_iese.name LIKE '%%electricidad%%')
-LEFT JOIN iv_21 ON iv_21.id = i.id
 LEFT JOIN ives ON ives.inv_id = i.id
 LEFT JOIN no_energy_invoices_with_vat ON no_energy_invoices_with_vat.inv_id = i.id
 LEFT JOIN no_energy_invoices_no_vat ON no_energy_invoices_no_vat.inv_id = i.id
